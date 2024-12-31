a752494a5a4dcf4ccb1c273cb36a15f6a7534ef8
Frozen <fmilesi_soft@hotmail.com>
2024-12-31 17:00:59 -0300

    Update main
    Update code to adapt to current project standards.

--- ps2/veronica/prog/njloop.c
+++ ps2/veronica/prog/njloop.c
@@ -1,19 +1,19 @@
 void main();
 
-
-
-
-
-
-
-/* 100% match */
-void main() { // Line 10, Address: 0x12b0a0
-  njUserInit(); // Line 11, Address: 0x12b0a8
-
-loop_1:
-  if (njUserMain() >= 0) { // Line 14, Address: 0x12b0b0
-    njWaitVSync(); // Line 15, Address: 0x12b0c0
-    goto loop_1; // Line 16, Address: 0x12b0c8
-  }
-  njUserExit(); // Line 18, Address: 0x12b0d0
-} // Line 19, Address: 0x12b0d8
+// 100% matching!
+void main() 
+{
+    njUserInit(); 
+    
+    while (TRUE) 
+    { 
+        if (njUserMain() < 0) 
+        { 
+            break;
+        } 
+        
+        njWaitVSync();
+    } 
+    
+    njUserExit(); 
+} 
