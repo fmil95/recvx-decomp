405c179e9b931cd9b9dcfbd79f050c7b65389af6
Frozen Burnside <fmilesi_soft@hotmail.com>
2025-03-17 21:28:48 -0300

    Decompile buGetDiskInfo

--- ps2/veronica/prog/ps2_sg_bup.c
+++ ps2/veronica/prog/ps2_sg_bup.c
@@ -1,100 +1,12 @@
-/*typedef struct _anon0;
-typedef struct _anon1;
-typedef struct _anon2;
-typedef struct _anon3;
-typedef struct _anon4;
-typedef struct _anon5;
-
-typedef int(*type_0)(int, int, int, unsigned int);
-typedef int(*type_1)(int, int, int, int);
-typedef void(*type_6)();
-
-typedef unsigned short type_2[4];
-typedef char type_3[18];
-typedef char type_4[34];
-typedef unsigned char type_5[16];
-typedef unsigned char type_7[32];
-
-struct _anon0
-{
-	unsigned int filesize;
-	unsigned short blocks;
-	unsigned char type;
-	unsigned char copyflag;
-	unsigned short headerofs;
-	_anon5 time;
-};
-
-struct _anon1
-{
-	char vms_comment[18];
-	char btr_comment[34];
-	unsigned char game_name[16];
-	void* icon_palette;
-	void* icon_data;
-	unsigned short icon_num;
-	unsigned short icon_speed;
-	void* visual_data;
-	unsigned short visual_type;
-	unsigned short reserved;
-	void* save_data;
-	unsigned int save_size;
-};
-
-struct _anon2
-{
-	_anon3 _Create;
-	_anon4 _Modify;
-	unsigned int FileSizeByte;
-	unsigned short AttrFile;
-	unsigned short Reserve1;
-	unsigned int Reserve2;
-	unsigned int PdaAplNo;
-	unsigned char EntryName[32];
-};
-
-struct _anon3
-{
-	unsigned char Resv2;
-	unsigned char Sec;
-	unsigned char Min;
-	unsigned char Hour;
-	unsigned char Day;
-	unsigned char Month;
-	unsigned short Year;
-};
-
-struct _anon4
-{
-	unsigned char Resv2;
-	unsigned char Sec;
-	unsigned char Min;
-	unsigned char Hour;
-	unsigned char Day;
-	unsigned char Month;
-	unsigned short Year;
-};
-
-struct _anon5
-{
-	unsigned short year;
-	unsigned char month;
-	unsigned char day;
-	unsigned char hour;
-	unsigned char minute;
-	unsigned char second;
-	unsigned char dayofweek;
-};
-
-unsigned short visula_size[4];*/
+unsigned short visula_size[4];
 int(*__bu_completecallback_func)(int, int, int, unsigned int);
 int(*__bu_progresscallback_func)(int, int, int, int);
 
-/*int buExit();
-int buGetDiskInfo();
-int buInit(void(*func)());
-void buSetCompleteCallback(int(*func)(int, int, int, unsigned int));
-void buSetProgressCallback(int(*func)(int, int, int, int));*/
+Sint32 buExit(void);
+Sint32 buGetDiskInfo(Sint32 drive, BUS_DISKINFO* info);
+Sint32 buInit(Sint32 capacity, Sint32 drives, void* work, BU_INITCALLBACK func);
+void buSetCompleteCallback(BU_COMPLETECALLBACK func);
+void buSetProgressCallback(BU_PROGRESSCALLBACK func);
 
 // 100% matching! 
 Sint32 buExit(void)
@@ -102,13 +14,10 @@ Sint32 buExit(void)
     return 0;
 }
 
-// 
-// Start address: 0x2d8c60
-int buGetDiskInfo()
+// 100% matching! 
+Sint32 buGetDiskInfo(Sint32 drive, BUS_DISKINFO* info)
 {
-	// Line 240, Address: 0x2d8c60, Func Offset: 0
-	// Func End, Address: 0x2d8c68, Func Offset: 0x8
-	scePrintf("buGetDiskInfo - UNIMPLEMENTED!\n");
+    return 0;
 }
 
 // 100% matching!
