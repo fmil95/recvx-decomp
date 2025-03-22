772436f2da15b138bd812834341c6e1fdff74b31
Frozen Burnside <fmilesi_soft@hotmail.com>
2025-03-22 11:23:51 -0300

    Decompile bhStFlg

--- ps2/veronica/prog/flag.c
+++ ps2/veronica/prog/flag.c
@@ -1,19 +1,12 @@
-
-
-
-
-/*void bhStFlg(unsigned int* flg, unsigned int bit);
+void bhStFlg(unsigned int* flg, unsigned int bit);
 void bhCrFlg(unsigned int* flg, unsigned int bit);
 int bhCkFlg(unsigned int* flg, unsigned int bit);
 
-// 
-// Start address: 0x280e40
+// 100% matching! 
 void bhStFlg(unsigned int* flg, unsigned int bit)
 {
-	// Line 37, Address: 0x280e40, Func Offset: 0
-	// Line 38, Address: 0x280e60, Func Offset: 0x20
-	// Func End, Address: 0x280e68, Func Offset: 0x28
-}*/
+    flg[bit / 32] |= 0x80000000 >> (bit & 31); 
+} 
 
 // 100% matching! 
 void bhCrFlg(unsigned int* flg, unsigned int bit)
