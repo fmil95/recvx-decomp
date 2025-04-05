50d74b76cd9296d7dc45d5adafb58464caa8d369
Frozen Burnside <fmilesi_soft@hotmail.com>
2025-04-05 18:41:53 -0300

    Decompile Pad_set + tidy up ps2_sg_pad.c

--- ps2/veronica/prog/ps2_sg_pad.c
+++ ps2/veronica/prog/ps2_sg_pad.c
@@ -1,136 +1,3 @@
-/*typedef struct _anon0;
-typedef struct _anon1;
-typedef struct _anon2;
-typedef struct _anon3;
-typedef struct _anon4;
-typedef struct _anon5;
-typedef struct _anon6;
-
-
-typedef unsigned char type_0[8];
-typedef char type_1[32];
-typedef char type_2[64];
-typedef <unknown fundamental type (0xa510)> type_3[16];
-typedef unsigned char type_4[32];
-typedef unsigned char type_5[32];
-typedef unsigned int type_6[2];
-typedef _anon5 type_7[5];
-typedef unsigned char type_8[2];
-typedef char type_9[16];
-typedef char type_10[16];
-typedef <unknown fundamental type (0xa510)> type_11[16];
-typedef _anon3 type_12[0];
-typedef unsigned int type_13[3];
-typedef unsigned short type_14[2];
-typedef unsigned char type_15[2];
-
-struct _anon0
-{
-	unsigned short on;
-	unsigned short old;
-	unsigned short push;
-	unsigned short release;
-	short send;
-	unsigned short ad_flag;
-	unsigned char buff[2];
-	unsigned short onon;
-	unsigned short repeat;
-	char time1[16];
-	char time2[16];
-	unsigned char rh;
-	unsigned char rv;
-	unsigned char lh;
-	unsigned char lv;
-	unsigned char au;
-	unsigned char ad;
-	unsigned char al;
-	unsigned char ar;
-	unsigned char abu;
-	unsigned char abd;
-	unsigned char abl;
-	unsigned char abr;
-	unsigned char al1;
-	unsigned char al2;
-	unsigned char ar1;
-	unsigned char ar2;
-};
-
-struct _anon1
-{
-	unsigned int id;
-	unsigned int support;
-	unsigned int on;
-	unsigned int off;
-	unsigned int press;
-	unsigned int release;
-	unsigned short r;
-	unsigned short l;
-	short x1;
-	short y1;
-	short x2;
-	short y2;
-	char* name;
-	void* extend;
-	unsigned int old;
-	_anon2* info;
-};
-
-struct _anon2
-{
-	unsigned int type;
-	unsigned int reserved[3];
-	unsigned char is_root;
-	unsigned char area_code;
-	unsigned char connector_dir[2];
-	char product_name[32];
-	char license[64];
-	unsigned short stdby_pow;
-	unsigned short max_pow;
-};
-
-struct _anon3
-{
-	int PerType;
-	int OldPerType;
-	int PerTypeEx;
-	unsigned int Flag;
-	int SoftReset;
-	unsigned int on;
-	unsigned int press;
-	unsigned int Rept;
-	unsigned int on2;
-	unsigned int on2old;
-	unsigned int press2;
-	unsigned short l;
-	unsigned short r;
-	short x1;
-	short y1;
-	short x2;
-	short y2;
-	short Calibrate;
-};
-
-struct _anon4
-{
-	_anon0 pad1;
-	_anon0 pad2;
-};
-
-struct _anon5
-{
-	int Type;
-	unsigned int Button;
-};
-
-struct _anon6
-{
-	unsigned short routine_0;
-	unsigned short type;
-	unsigned short act_lv[2];
-	unsigned short be_flag;
-	unsigned char act_data[8];
-};*/
-
 unsigned int Old_sys_cnt;
 static u_long128 Padd1[scePadDmaBufferMax];
 static u_long128 Padd2[scePadDmaBufferMax];
@@ -154,19 +21,19 @@ unsigned int Pad_state[2];
 int SoftResetFlag;
 PAD_INFO Pad[4];
 int CurrentPortId;
-/*_anon4 Pad_info;
-_anon6 Pad_status2;
+PAD_WRK Pad_info;
+NO_NAME_19 Pad_status2;
 
-void pdInitPeripheral();
-void pdExitPeripheral();
-_anon1* pdGetPeripheral(unsigned int port);
-_anon2* pdGetPeripheralInfo();
-void pdSetMode();*/
+void pdInitPeripheral(Sint32 logic, void* recvbuf, void* sendbuf);
+void pdExitPeripheral(void);
+const PDS_PERIPHERAL* pdGetPeripheral(Uint32 port);
+const PDS_PERIPHERALINFO* pdGetPeripheralInfo(Uint32 port);
+void pdSetMode(Sint32 mode);
 void Ps2_pad_read();
 void Ps2_Read_Key(PDS_PERIPHERAL* per, PAD_WORK* pad_wk);
 void Ps2_MakeRepeatKey(unsigned int Id, PAD_WORK* pad_wk);
 void Pad_set(PAD_WORK* pbt, unsigned short pad_num);
-/*void Pad_init();*/
+void Pad_init();
 
 // 100% matching!
 void pdInitPeripheral(Sint32 logic, void* recvbuf, void* sendbuf)
@@ -651,86 +518,126 @@ void Ps2_MakeRepeatKey(unsigned int Id, PAD_WORK* pad_wk)
     Pad[Id].Rept = pad_wk->onon;
 }
 
-// 
-// Start address: 0x2da7a0
+// 100% matching!
 void Pad_set(PAD_WORK* pbt, unsigned short pad_num)
 {
-	unsigned char* pad_data;
-	short i;
-	// Line 875, Address: 0x2da7a0, Func Offset: 0
-	// Line 712, Address: 0x2da7a4, Func Offset: 0x4
-	// Line 875, Address: 0x2da7a8, Func Offset: 0x8
-	// Line 876, Address: 0x2da7b4, Func Offset: 0x14
-	// Line 879, Address: 0x2da7bc, Func Offset: 0x1c
-	// Line 880, Address: 0x2da7c4, Func Offset: 0x24
-	// Line 883, Address: 0x2da7d8, Func Offset: 0x38
-	// Line 884, Address: 0x2da7e8, Func Offset: 0x48
-	// Line 885, Address: 0x2da7f4, Func Offset: 0x54
-	// Line 886, Address: 0x2da804, Func Offset: 0x64
-	// Line 887, Address: 0x2da810, Func Offset: 0x70
-	// Line 888, Address: 0x2da820, Func Offset: 0x80
-	// Line 889, Address: 0x2da82c, Func Offset: 0x8c
-	// Line 890, Address: 0x2da83c, Func Offset: 0x9c
-	// Line 893, Address: 0x2da848, Func Offset: 0xa8
-	// Line 913, Address: 0x2da850, Func Offset: 0xb0
-	// Line 893, Address: 0x2da85c, Func Offset: 0xbc
-	// Line 894, Address: 0x2da868, Func Offset: 0xc8
-	// Line 895, Address: 0x2da87c, Func Offset: 0xdc
-	// Line 896, Address: 0x2da884, Func Offset: 0xe4
-	// Line 897, Address: 0x2da88c, Func Offset: 0xec
-	// Line 898, Address: 0x2da894, Func Offset: 0xf4
-	// Line 900, Address: 0x2da89c, Func Offset: 0xfc
-	// Line 901, Address: 0x2da8a4, Func Offset: 0x104
-	// Line 902, Address: 0x2da8ac, Func Offset: 0x10c
-	// Line 903, Address: 0x2da8b4, Func Offset: 0x114
-	// Line 904, Address: 0x2da8bc, Func Offset: 0x11c
-	// Line 905, Address: 0x2da8c4, Func Offset: 0x124
-	// Line 906, Address: 0x2da8cc, Func Offset: 0x12c
-	// Line 907, Address: 0x2da8d4, Func Offset: 0x134
-	// Line 908, Address: 0x2da8dc, Func Offset: 0x13c
-	// Line 909, Address: 0x2da8e4, Func Offset: 0x144
-	// Line 910, Address: 0x2da8ec, Func Offset: 0x14c
-	// Line 911, Address: 0x2da8f4, Func Offset: 0x154
-	// Line 926, Address: 0x2da8fc, Func Offset: 0x15c
-	// Line 916, Address: 0x2da900, Func Offset: 0x160
-	// Line 919, Address: 0x2da904, Func Offset: 0x164
-	// Line 932, Address: 0x2da908, Func Offset: 0x168
-	// Line 934, Address: 0x2da90c, Func Offset: 0x16c
-	// Line 915, Address: 0x2da910, Func Offset: 0x170
-	// Line 916, Address: 0x2da934, Func Offset: 0x194
-	// Line 917, Address: 0x2da938, Func Offset: 0x198
-	// Line 916, Address: 0x2da93c, Func Offset: 0x19c
-	// Line 917, Address: 0x2da940, Func Offset: 0x1a0
-	// Line 919, Address: 0x2da950, Func Offset: 0x1b0
-	// Line 920, Address: 0x2da954, Func Offset: 0x1b4
-	// Line 921, Address: 0x2da95c, Func Offset: 0x1bc
-	// Line 922, Address: 0x2da970, Func Offset: 0x1d0
-	// Line 923, Address: 0x2da980, Func Offset: 0x1e0
-	// Line 924, Address: 0x2da984, Func Offset: 0x1e4
-	// Line 923, Address: 0x2da98c, Func Offset: 0x1ec
-	// Line 924, Address: 0x2da994, Func Offset: 0x1f4
-	// Line 925, Address: 0x2da99c, Func Offset: 0x1fc
-	// Line 926, Address: 0x2da9a4, Func Offset: 0x204
-	// Line 927, Address: 0x2da9a8, Func Offset: 0x208
-	// Line 930, Address: 0x2da9b8, Func Offset: 0x218
-	// Line 931, Address: 0x2da9c4, Func Offset: 0x224
-	// Line 932, Address: 0x2da9c8, Func Offset: 0x228
-	// Line 931, Address: 0x2da9d0, Func Offset: 0x230
-	// Line 932, Address: 0x2da9d8, Func Offset: 0x238
-	// Line 933, Address: 0x2da9e0, Func Offset: 0x240
-	// Line 934, Address: 0x2da9e8, Func Offset: 0x248
-	// Line 935, Address: 0x2da9ec, Func Offset: 0x24c
-	// Line 937, Address: 0x2da9f8, Func Offset: 0x258
-	// Line 938, Address: 0x2daa00, Func Offset: 0x260
-	// Line 939, Address: 0x2daa10, Func Offset: 0x270
-	// Line 938, Address: 0x2daa14, Func Offset: 0x274
-	// Line 939, Address: 0x2daa1c, Func Offset: 0x27c
-	// Line 941, Address: 0x2daa20, Func Offset: 0x280
-	// Line 942, Address: 0x2daa2c, Func Offset: 0x28c
-	// Line 944, Address: 0x2daa30, Func Offset: 0x290
-	// Line 946, Address: 0x2daa44, Func Offset: 0x2a4
-	// Func End, Address: 0x2daa4c, Func Offset: 0x2ac
-	scePrintf("Pad_set - UNIMPLEMENTED!\n");
+    short i; 
+    unsigned char* pad_data; 
+	
+    pad_data = Pad_rdata1;
+    
+    if (pad_num == 2) 
+    {
+        pad_data = Pad_rdata2;
+    }
+    
+    pbt->old = pbt->on;
+    
+    pbt->on = (pad_data[2] << 8) | pad_data[3];
+    
+    if (pbt->lh > 192) 
+    {
+        pbt->on |= 0x8000;
+    }
+    
+    if (pbt->lh < 64) 
+    {
+        pbt->on |= 0x2000;
+    }
+    
+    if (pbt->lv > 192)
+    {
+        pbt->on |= 0x1000;
+    }
+    
+    if (pbt->lv < 64) 
+    {
+        pbt->on |= 0x4000;
+    }
+    
+    pbt->push = pbt->on & (pbt->on ^ pbt->old);
+    
+    pbt->release = pbt->old & (pbt->on ^ pbt->old);
+    
+    pbt->rh = pad_data[4];
+    pbt->rv = pad_data[5];
+    
+    pbt->lh = pad_data[6];
+    pbt->lv = pad_data[7];
+    
+    pbt->ar = pad_data[8];
+    pbt->al = pad_data[9];
+    
+    pbt->au = pad_data[10];
+    pbt->ad = pad_data[11];
+    
+    pbt->abu = pad_data[12];
+    pbt->abr = pad_data[13];
+    
+    pbt->abd = pad_data[14];
+    pbt->abl = pad_data[15];
+    
+    pbt->al1 = pad_data[16];
+    pbt->al2 = pad_data[17];
+    
+    pbt->ar1 = pad_data[18];
+    pbt->ar2 = pad_data[19];
+    
+    i = 15;
+    
+    do 
+    {
+        if ((1 << i) & ((pbt->on & pbt->push))) 
+        {
+            pbt->time1[i] = 10;
+            
+            pbt->onon |= 1 << i;
+            
+            pbt->time2[i] = 20;
+            
+            pbt->repeat |= 1 << i;
+        } 
+        else if ((pbt->on & (1 << i))) 
+        { 
+            if (pbt->time1[i] != 0) 
+            {
+                pbt->time1[i]--;
+                
+                pbt->onon &= ~(1 << i);
+            } 
+            else
+            {
+                pbt->time1[i] = 2;
+                
+                pbt->onon |= 1 << i;
+            }
+            
+            if (pbt->time2[i] != 0) 
+            {
+                pbt->time2[i]--;
+                
+                pbt->repeat &= ~(1 << i);
+            } 
+            else 
+            {
+                pbt->time2[i] = 4;
+                
+                pbt->repeat |= 1 << i;
+            }
+        } 
+        else 
+        {
+            pbt->onon &= ~(1 << i);
+            
+            pbt->time1[i] = 0;
+            
+            pbt->repeat &= ~(1 << i);
+            
+            pbt->time2[i] = 0;
+        }
+        
+        i--;
+    } while (i >= 0);
 }
 
 // 100% matching! 
