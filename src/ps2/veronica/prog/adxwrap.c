551219c7bba419c480799d1f91a237590898862c
Frozen Burnside <fmilesi_soft@hotmail.com>
2025-04-17 18:57:41 -0300

    Create adxwrap header

--- src/ps2/veronica/prog/adxwrap.c
+++ src/ps2/veronica/prog/adxwrap.c
@@ -1,3 +1,5 @@
+#include "adxwrap.h"
+
 char last_rdx_files[256];
 char last_adx_files[256];
 char last_file_id;
@@ -19,40 +21,6 @@ int rdx_image_data_max = 205;
 char* rdx_files[205];
 int ADX_STREAM_BUFF_OFFSET[2] = { 0, 307456 }; 
 char ADX_STREAM_BUFFER[471040];
-
-void InitAdx();
-void ExitAdx();
-void DeletePartition(unsigned int PartitionId);
-int CreatePartitionEx(AFS_PATINFO* ap);
-void DeletePartitionEx(AFS_PATINFO* ap);
-int SearchAdxFSlot();
-int OpenAfsInsideFile(unsigned int PartitionId, unsigned int FileId);
-int OpenAfsIsoFile(char* FileName);
-int GetAfsInsideFileSize(int SlotNo);
-void RequestReadAfsInsideFile(int SlotNo, unsigned char* Address);
-int CheckReadEndAfsInsideFile(int SlotNo);
-void CloseAfsInsideFile(int SlotNo);
-void StopAfsInsideFile(int SlotNo);
-void RegistAdxStreamEx(int MaxStream, int DummyStream, ADX_WORK* pAdx);
-void FreeAdxStream();
-void SleepAdxStream();
-void WakeupAdxStream(ADX_WORK* pAdx);
-void PlayAdxEx(unsigned int SlotNo, unsigned int PartitionId, unsigned int FileId, int Flag);
-void PlayAdx(unsigned int SlotNo, unsigned int PartitionId, unsigned int FileId);
-void StopAdx(unsigned int SlotNo);
-void PauseAdx(unsigned int SlotNo);
-void ContinueAdx(unsigned int SlotNo);
-int GetAdxStatus(unsigned int SlotNo);
-void SetVolumeAdx(unsigned int SlotNo, int Volume);
-void SetVolumeAdxEx(unsigned int SlotNo, float Volume, float MaxVolume);
-void SetVolumeAdx2(unsigned int SlotNo, float Volume);
-void SetPanAdx2(unsigned int SlotNo, int Channel, float Pan); // second parameter is not present on the debugging symbols
-void SetPanAdx(unsigned int SlotNo, int Channel, int Pan);
-int GetAdxPlayTime(unsigned int SlotNo);
-void RequestAdxFadeFunction2(int SlotNo, int Func, int Timer, int FirstVolume);
-void RequestAdxFadeFunction(int SlotNo, int Func, int Timer);
-void RequestAdxFadeFunctionEx(int SlotNo, int StartVol, int LastVol, int Frame);
-int ExecAdxFadeManager();
 
 // 100% matching!
 void InitAdx()
