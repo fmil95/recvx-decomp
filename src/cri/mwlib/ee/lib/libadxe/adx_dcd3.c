4dac08ead02236d6271757f104db998fd2be3ba7
Frozen Burnside <fmilesi_soft@hotmail.com>
2025-04-17 15:03:16 -0300

    Decompile ADX_SetDecodeSteAsMonoSw

--- src/cri/mwlib/ee/lib/libadxe/adx_dcd3.c
+++ src/cri/mwlib/ee/lib/libadxe/adx_dcd3.c
@@ -1,9 +1,12 @@
+Sint32 adx_decode_output_mono_flag;
+
 // ADX_DecodeMonoFloat
 // ADX_DecodeSteFloat
 // ADX_DecodeSteFloatAsMono
 // ADX_DecodeSteFloatAsSte
 
+// 100% matching!
 void ADX_SetDecodeSteAsMonoSw(Sint32 flag)
 {
-    scePrintf("ADX_SetDecodeSteAsMonoSw - UNIMPLEMENTED!\n");
+    adx_decode_output_mono_flag = flag;
 }
