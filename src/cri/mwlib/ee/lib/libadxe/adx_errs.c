66c9f3a4f640910fa7e45e3944ef9f277e2d496b
Frozen Burnside <fmilesi_soft@hotmail.com>
2025-04-22 23:28:59 -0300

    Decompile ADXERR_CallErrFunc1

--- src/cri/mwlib/ee/lib/libadxe/adx_errs.c
+++ src/cri/mwlib/ee/lib/libadxe/adx_errs.c
@@ -1,7 +1,16 @@
+void (*adxerr_func)(void* adxerr_obj, Char8* adxerr_msg);
+Char8 adxerr_msg[255];
+void* adxerr_obj;
 
-void ADXERR_CallErrFunc1(Char8* err_msg)
+// 100% matching!
+void ADXERR_CallErrFunc1(Char8* msg) 
 {
-    scePrintf("ADXERR_CallErrFunc1 - UNIMPLEMENTED!\n");
+    strncpy(adxerr_msg, msg, 255);
+
+    if (adxerr_func != NULL)
+    {
+        adxerr_func(adxerr_obj, adxerr_msg);
+    }
 }
 
 // ADXERR_CallErrFunc2
