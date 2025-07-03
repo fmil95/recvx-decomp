26991752855c41b9f1db29ff7421f0a529e72517
Frozen Burnside <fmilesi_soft@hotmail.com>
2025-07-03 15:35:28 -0300

    Decompile Expand
    https: //decomp.me/scratch/q7QrT
    Co-Authored-By: Mc-muffin <8714476+Mc-muffin@users.noreply.github.com>

--- src/ps2/veronica/prog/expand.c
+++ src/ps2/veronica/prog/expand.c
@@ -1,60 +1,7 @@
-/*typedef struct _anon0;
-typedef struct _anon1;
-typedef union _anon2;
-typedef struct _anon3;
-typedef struct _anon4;
-typedef union _anon5;
-
-
-typedef _anon0 type[2];
-
-struct _anon0
-{
-	char* from;
-	unsigned char* to;
-};
-
-struct _anon1
-{
-	char active;
-	char mode;
-};
-
-union _anon2
-{
-	_anon1 sb;
-	short check;
-};
-
-struct _anon3
-{
-	_anon5 flag;
-	int code;
-	int counter;
-	int repeat;
-	unsigned char* offset;
-	char* source;
-	unsigned char* destination;
-	_anon0 que[2];
-};
-
-struct _anon4
-{
-	_anon2 uw;
-	char in;
-	char out;
-};
-
-union _anon5
-{
-	_anon4 sl;
-	int abort;
-};*/
-
 EXPAND_CTRL_BUF ExpandCtrlBuf;
 
-/*void Init_Expand();
-int Expand(char* s, unsigned char* d);*/
+void Init_Expand();
+int Expand(register char* s, register unsigned char* d);
 
 // 100% matching!
 void Init_Expand() 
@@ -62,103 +9,195 @@ void Init_Expand()
     ExpandCtrlBuf.flag.abort = 0; 
 }
 
-// 
-// Start address: 0x2caea0
-int Expand(char* s, unsigned char* d)
+// 99.63% matching
+int Expand(register char* s, register unsigned char* d) 
 {
-	int T;
-	// Line 147, Address: 0x2caea0, Func Offset: 0
-	// Line 148, Address: 0x2caea4, Func Offset: 0x4
-	// Line 149, Address: 0x2caeac, Func Offset: 0xc
-	// Line 150, Address: 0x2caeb0, Func Offset: 0x10
-	// Line 151, Address: 0x2caeb8, Func Offset: 0x18
-	// Line 152, Address: 0x2caebc, Func Offset: 0x1c
-	// Line 153, Address: 0x2caec0, Func Offset: 0x20
-	// Line 154, Address: 0x2caec4, Func Offset: 0x24
-	// Line 155, Address: 0x2caec8, Func Offset: 0x28
-	// Line 157, Address: 0x2caecc, Func Offset: 0x2c
-	// Line 159, Address: 0x2caed4, Func Offset: 0x34
-	// Line 160, Address: 0x2caed8, Func Offset: 0x38
-	// Line 161, Address: 0x2caedc, Func Offset: 0x3c
-	// Line 162, Address: 0x2caee0, Func Offset: 0x40
-	// Line 164, Address: 0x2caee4, Func Offset: 0x44
-	// Line 165, Address: 0x2caee8, Func Offset: 0x48
-	// Line 167, Address: 0x2caeec, Func Offset: 0x4c
-	// Line 168, Address: 0x2caef4, Func Offset: 0x54
-	// Line 169, Address: 0x2caef8, Func Offset: 0x58
-	// Line 170, Address: 0x2caefc, Func Offset: 0x5c
-	// Line 171, Address: 0x2caf00, Func Offset: 0x60
-	// Line 172, Address: 0x2caf04, Func Offset: 0x64
-	// Line 173, Address: 0x2caf08, Func Offset: 0x68
-	// Line 174, Address: 0x2caf0c, Func Offset: 0x6c
-	// Line 176, Address: 0x2caf10, Func Offset: 0x70
-	// Line 178, Address: 0x2caf18, Func Offset: 0x78
-	// Line 179, Address: 0x2caf1c, Func Offset: 0x7c
-	// Line 181, Address: 0x2caf20, Func Offset: 0x80
-	// Line 182, Address: 0x2caf28, Func Offset: 0x88
-	// Line 183, Address: 0x2caf2c, Func Offset: 0x8c
-	// Line 184, Address: 0x2caf30, Func Offset: 0x90
-	// Line 185, Address: 0x2caf34, Func Offset: 0x94
-	// Line 186, Address: 0x2caf38, Func Offset: 0x98
-	// Line 187, Address: 0x2caf3c, Func Offset: 0x9c
-	// Line 188, Address: 0x2caf40, Func Offset: 0xa0
-	// Line 190, Address: 0x2caf44, Func Offset: 0xa4
-	// Line 191, Address: 0x2caf4c, Func Offset: 0xac
-	// Line 192, Address: 0x2caf50, Func Offset: 0xb0
-	// Line 193, Address: 0x2caf54, Func Offset: 0xb4
-	// Line 195, Address: 0x2caf58, Func Offset: 0xb8
-	// Line 196, Address: 0x2caf60, Func Offset: 0xc0
-	// Line 197, Address: 0x2caf64, Func Offset: 0xc4
-	// Line 198, Address: 0x2caf68, Func Offset: 0xc8
-	// Line 199, Address: 0x2caf6c, Func Offset: 0xcc
-	// Line 200, Address: 0x2caf70, Func Offset: 0xd0
-	// Line 201, Address: 0x2caf74, Func Offset: 0xd4
-	// Line 202, Address: 0x2caf78, Func Offset: 0xd8
-	// Line 204, Address: 0x2caf7c, Func Offset: 0xdc
-	// Line 205, Address: 0x2caf80, Func Offset: 0xe0
-	// Line 206, Address: 0x2caf84, Func Offset: 0xe4
-	// Line 207, Address: 0x2caf88, Func Offset: 0xe8
-	// Line 209, Address: 0x2caf8c, Func Offset: 0xec
-	// Line 210, Address: 0x2caf94, Func Offset: 0xf4
-	// Line 211, Address: 0x2caf98, Func Offset: 0xf8
-	// Line 212, Address: 0x2caf9c, Func Offset: 0xfc
-	// Line 213, Address: 0x2cafa0, Func Offset: 0x100
-	// Line 214, Address: 0x2cafa4, Func Offset: 0x104
-	// Line 215, Address: 0x2cafa8, Func Offset: 0x108
-	// Line 216, Address: 0x2cafac, Func Offset: 0x10c
-	// Line 218, Address: 0x2cafb0, Func Offset: 0x110
-	// Line 219, Address: 0x2cafb4, Func Offset: 0x114
-	// Line 220, Address: 0x2cafb8, Func Offset: 0x118
-	// Line 221, Address: 0x2cafbc, Func Offset: 0x11c
-	// Line 222, Address: 0x2cafc0, Func Offset: 0x120
-	// Line 224, Address: 0x2cafc4, Func Offset: 0x124
-	// Line 226, Address: 0x2cafc8, Func Offset: 0x128
-	// Line 228, Address: 0x2cafcc, Func Offset: 0x12c
-	// Line 229, Address: 0x2cafd0, Func Offset: 0x130
-	// Line 230, Address: 0x2cafd4, Func Offset: 0x134
-	// Line 231, Address: 0x2cafd8, Func Offset: 0x138
-	// Line 232, Address: 0x2cafdc, Func Offset: 0x13c
-	// Line 233, Address: 0x2cafe0, Func Offset: 0x140
-	// Line 236, Address: 0x2cafe8, Func Offset: 0x148
-	// Line 240, Address: 0x2caff0, Func Offset: 0x150
-	// Line 241, Address: 0x2caff4, Func Offset: 0x154
-	// Line 242, Address: 0x2caff8, Func Offset: 0x158
-	// Line 243, Address: 0x2caffc, Func Offset: 0x15c
-	// Line 244, Address: 0x2cb000, Func Offset: 0x160
-	// Line 245, Address: 0x2cb004, Func Offset: 0x164
-	// Line 246, Address: 0x2cb008, Func Offset: 0x168
-	// Line 247, Address: 0x2cb00c, Func Offset: 0x16c
-	// Line 248, Address: 0x2cb014, Func Offset: 0x174
-	// Line 250, Address: 0x2cb018, Func Offset: 0x178
-	// Line 251, Address: 0x2cb01c, Func Offset: 0x17c
-	// Line 253, Address: 0x2cb020, Func Offset: 0x180
-	// Line 254, Address: 0x2cb028, Func Offset: 0x188
-	// Line 255, Address: 0x2cb02c, Func Offset: 0x18c
-	// Line 256, Address: 0x2cb030, Func Offset: 0x190
-	// Line 257, Address: 0x2cb034, Func Offset: 0x194
-	// Line 259, Address: 0x2cb038, Func Offset: 0x198
-	// Line 262, Address: 0x2cb040, Func Offset: 0x1a0
-	// Line 353, Address: 0x2cb044, Func Offset: 0x1a4
-	// Func End, Address: 0x2cb04c, Func Offset: 0x1ac
-	scePrintf("Expand - UNIMPLEMENTED!\n");
-}
+    int T;
+
+    asm volatile 
+    {
+    
+        lui     t9, 0xFFFF 
+        addiu   t9, t9, 0x7F08
+        addiu   t9, t9, 0x7FF8
+            
+        lui     t8, 0xFFFF 
+        addiu   t8, t8, 0x6008
+        addiu   t8, t8, 0x7FF8 
+            
+        lbu     t6, 0(s) 
+        
+        addi    s,  s,  1 
+        
+        sub     t7, t7, t7 
+        addiu   t7, t7, 9 
+        sub     t3, t3, t3 
+        addu    t3, t3, d
+        
+        j       l_002CAEE4 
+        
+        l_002CAED4:
+        lbu     t1, 0(s) 
+        
+        addi    s, s, 1 
+        
+        sb      t1, 0(d) 
+            
+        addi    d, d, 1 
+        
+        l_002CAEE4:
+        addiu   t7, t7, -1 
+        
+        andi    t4, t6, 0x1 
+        srl     t6, t6, 1
+        
+        bgtz    t7, l_002CAF10         
+        vnop   
+        
+        lbu     t6, 0(s) 
+        
+        addi    s,  s,  1 
+        
+        sub     t7, t7, t7 
+        addiu   t7, t7, 8 
+        
+        andi    t4, t6, 0x1 
+            
+        srl     t6, t6, 1 
+            
+        l_002CAF10:
+        bgtz    t4, l_002CAED4 
+    
+        addiu   t7, t7, -1 
+            
+        andi    t4, t6, 0x1 
+        srl     t6, t6, 1 
+        
+        bgtz    t7, l_002CAF44 
+        vnop
+        
+        lbu     t6, 0(s)
+            
+        addi    s,  s,  1 
+        
+        sub     t7, t7, t7 
+        addiu   t7, t7, 8 
+        
+        andi    t4, t6, 0x1 
+            
+        srl     t6, t6, 1 
+            
+        l_002CAF44:
+        bgtz    t4, l_002CAFF0 
+        
+        sub     t0, t0, t0 
+        
+        addiu   t7, t7, -1 
+            
+        andi    t4, t6, 0x1 
+        srl     t6, t6, 1
+            
+        bgtz    t7, l_002CAF7C 
+        vnop    
+        
+        lbu     t6, 0(s) 
+            
+        addi    s,  s,  1 
+        
+        sub     t7, t7, t7 
+        addiu   t7, t7, 8 
+        andi    t4, t6, 0x1 
+        srl     t6, t6, 1 
+            
+        l_002CAF7C:
+        sll     t0, t0, 1 
+        or      t0, t0, t4 
+            
+        addiu   t7, t7, -1 
+            
+        andi    t4, t6, 0x1 
+        srl     t6, t6, 1
+            
+        bgtz    t7, l_002CAFB0 
+        vnop     
+        
+        lbu     t6, 0(s)
+            
+        addi    s,  s,  1 
+        
+        sub     t7, t7, t7 
+        addiu   t7, t7, 8 
+        andi    t4, t6, 0x1 
+        srl     t6, t6, 1 
+            
+        l_002CAFB0:
+        lbu     t2, 0(s)
+        
+        addi    s,  s,  1 
+        
+        sll     t0, t0, 1 
+        or      t0, t0, t4 
+        or      t2, t2, t9 
+    
+        l_002CAFC4:
+        addiu   t0, t0, 2 
+        addu    t2, d,  t2 
+            
+        l_002CAFCC:
+        lbu     t1, 0(t2) 
+            
+        addiu   t2, t2, 1 
+        
+        addiu   t0, t0, -1 
+            
+        sb      t1, 0(d) 
+            
+        addi    d, d, 1 
+        nop
+        
+        bgtz    t0, l_002CAFCC 
+        vnop    
+        
+        j       l_002CAEE4 
+        vnop    
+    
+        l_002CAFF0:
+        lbu     t0, 0(s) 
+        lbu     t1, 1(s)
+            
+        addi    s,  s,  2 
+        
+        andi    t2, t0, 0xFF 
+        sll     t1, t1, 8 
+        or      t2, t2, t1 
+            
+        sub     t4, t4, t4 
+        
+        beq     t4, t2, l_002CB040 
+        
+        srl     t2, t2, 3 
+        andi    t0, t0, 0x7 
+            
+        and     t4, t4, t4 
+            
+        or      t2, t2, t8
+        
+        bne     t4, t0, l_002CAFC4 
+        
+        lbu     t0, 0(s) 
+            
+        addi    s,  s,  1 
+        
+        addu    t2, d,  t2 
+        andi    t0, t0, 0xFF 
+        addiu   t0, t0, 1
+            
+        j       l_002CAFCC 
+        nop
+        
+        l_002CB040:
+        sub     v0, d, t3 
+        nop
+        
+    }	
+} 
