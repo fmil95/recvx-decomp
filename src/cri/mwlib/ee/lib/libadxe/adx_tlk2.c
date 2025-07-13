2935058dd3fbc399611f420dae4f00204c7d3a9e
Frozen Burnside <fmilesi_soft@hotmail.com>
2025-07-13 19:43:11 -0300

    Decompile ADXT_StartMem

--- src/cri/mwlib/ee/lib/libadxe/adx_tlk2.c
+++ src/cri/mwlib/ee/lib/libadxe/adx_tlk2.c
@@ -6,6 +6,16 @@ void ADXT_StartAfs(ADXT adxt, Sint32 patid, Sint32 fid)
 }
 
 // ADXT_StartFname
-// ADXT_StartMem
-// ADXT_StartMem2
+
+// 100% matching!
+void ADXT_StartMem(ADXT adxt, void *adxdat)
+{
+    ADXT_StartMem2(adxt, adxdat, ADXT_MAX_DATASIZE);
+}
+
+void ADXT_StartMem2(ADXT adxt, void *adxdat, Sint32 datlen)
+{
+    scePrintf("ADXT_StartMem2 - UNIMPLEMENTED!\n");
+}
+
 // ADXT_StartMemIdx
