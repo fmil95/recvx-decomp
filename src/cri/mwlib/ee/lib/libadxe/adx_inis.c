1f9bad3cb3c2f42ee8711584e86414279f866e41
Frozen Burnside <fmilesi_soft@hotmail.com>
2025-07-22 20:06:56 -0300

    Decompile ADXT_ConfigVsyncSvr

--- src/cri/mwlib/ee/lib/libadxe/adx_inis.c
+++ src/cri/mwlib/ee/lib/libadxe/adx_inis.c
@@ -1,4 +1,4 @@
-
+Sint32 adxt_vsync_svr_flag;
 
 // 100% matching!
 void adxini_rnaerr_cbfn(void* arg0, Char8* err_msg) 
@@ -6,7 +6,11 @@ void adxini_rnaerr_cbfn(void* arg0, Char8* err_msg)
     ADXERR_CallErrFunc1(err_msg);
 }
 
-// ADXT_ConfigVsyncSvr
+// 100% matching!
+void ADXT_ConfigVsyncSvr(Sint32 flg) 
+{
+    adxt_vsync_svr_flag = flg;
+}
 
 void ADXT_Finish(void) 
 {
