4796551cef3ad891cb687dd28e393251d65485f7
Frozen Burnside <fmilesi_soft@hotmail.com>
2025-07-24 11:38:48 -0300

    Decompile LSC_LockCrs

--- src/cri/mwlib/ee/lib/libadxe/lsc_crs.c
+++ src/cri/mwlib/ee/lib/libadxe/lsc_crs.c
@@ -1,6 +1,10 @@
 
 
-// LSC_LockCrs
+// 0% matching - devs made a mistake and defined this function to return an int
+void LSC_LockCrs(void) 
+{
+    SJCRS_Lock(); 
+}
 
 // 100% matching!
 void LSC_UnlockCrs(void) 
