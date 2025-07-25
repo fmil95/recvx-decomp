b37758e77f7c86888ef9e1779e0c138cb1b0d1af
Frozen Burnside <fmilesi_soft@hotmail.com>
2025-07-25 17:40:50 -0300

    Decompile ADXF_Init

--- src/cri/mwlib/ee/lib/libadxe/adx_fini.c
+++ src/cri/mwlib/ee/lib/libadxe/adx_fini.c
@@ -1,11 +1,32 @@
-
+volatile Sint32 adxf_build;
+Sint32 adxf_init_cnt;
 
 void ADXF_Finish(void)
 {
     scePrintf("ADXF_Finish - UNIMPLEMENTED!\n");
 }
 
-void ADXF_Init(void)
+// 100% matching!
+void ADXF_Init(void) 
 {
-    scePrintf("ADXF_Init - UNIMPLEMENTED!\n");
-}
+    adxf_build;
+    
+    if (adxf_init_cnt == 0)
+    {
+        memset(adxf_obj, 0, sizeof(adxf_obj));
+        memset(adxf_ptinfo, 0, sizeof(adxf_ptinfo)); 
+        memset(adxf_cmd_hstry, ADXF_CMD_HSTRY_MAX - 1, sizeof(adxf_cmd_hstry));
+        memset(adxf_cmd_ncall, 0, sizeof(adxf_cmd_ncall));
+        
+        adxf_hstry_no = 0;
+        
+        adxf_ocbi_fg = 0;
+        
+        adxf_flno = 0;
+        
+        adxf_ldptnw_hn = 0;
+        adxf_ldptnw_ptid = -1; 
+    }
+    
+    adxf_init_cnt++;
+} 
