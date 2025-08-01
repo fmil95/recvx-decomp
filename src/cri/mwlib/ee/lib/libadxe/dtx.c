e66d7d8eb195519ef632505c4780c140c54fe4e8
Frozen Burnside <fmilesi_soft@hotmail.com>
2025-08-01 11:58:55 -0300

    Decompile DTX_Close

--- src/cri/mwlib/ee/lib/libadxe/dtx.c
+++ src/cri/mwlib/ee/lib/libadxe/dtx.c
@@ -1,11 +1,38 @@
 
+typedef struct _dtx 
+{
+    Sint32 unk0;
+    Sint32 unk4;
+    Sint32 unk8;
+    Sint32 unkC;
+    Sint32 unk10;
+    Sint32 unk14;
+    Sint32 unk18;
+    Sint32 unk1C;
+    Sint32 unk20;
+    Sint32 unk24;
+    Sint32 unk28;
+    Sint32 unk2C;
+} DTX_OBJ;
+
+typedef DTX_OBJ *DTX;
 
 void* DTX_CallUrpc(Sint32 arg0, void* sjrtm, Sint32 arg2, void* arg3, Sint32 arg4)
 {
     scePrintf("DTX_CallUrpc - UNIMPLEMENTED!\n");
 }
 
-// DTX_Close
+// 100% matching!
+void DTX_Close(DTX dtx) 
+{
+    dtx->unk20 = 0;
+    
+    dtx->unk24 = 0;
+    
+    dtx->unk28 = 0;
+    
+    dtx->unk2C = 0;
+}
 
 void* DTX_Create(Sint32 maxnch, void* eewk, void* iopwk, Sint32 wklen)
 {
