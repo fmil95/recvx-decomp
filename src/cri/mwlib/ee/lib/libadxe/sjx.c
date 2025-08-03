84d08a929f2b40e0f04885a518a82f4d5742478b
Frozen Burnside <fmilesi_soft@hotmail.com>
2025-08-03 17:54:54 -0300

    Decompile SJX_Init

--- src/cri/mwlib/ee/lib/libadxe/sjx.c
+++ src/cri/mwlib/ee/lib/libadxe/sjx.c
@@ -1,5 +1,12 @@
-
 static Sint32 sjx_init_cnt;
+static Sint32 sjx_wklen;
+static void* sjx_eewk;
+static Sint32 sjx_ee_work[564];
+static void* sjx_iopwk;
+static DTX sjx_dtx;
+
+void sjx_rcvcbf(void* sjx, void* buf, Sint32 bfsize);
+void sjx_sndcbf(void* sjx, void* buf, Sint32 bfsize);
 
 void* SJX_Create(SJ sj, Sint8 *work, Sint32 wksize)
 {
@@ -23,11 +30,55 @@ void SJX_Finish(void)
     sjx_init_cnt--;
 }
 
+// 100% matching!
 void SJX_Init(void)
 {
-    scePrintf("SJX_Init - UNIMPLEMENTED!\n");
+    if (sjx_init_cnt == 0)
+    {
+        DTX_Init();
+        
+        sjx_wklen = 2176;
+        
+        sjx_eewk = (void*)(((Uint32)sjx_ee_work + 64) & ~0x3F);
+
+        if (sjx_iopwk == NULL) 
+        {
+            sjx_iopwk = sceSifAllocIopHeap(2256);
+
+            if (sjx_iopwk == NULL) 
+            {
+                printf("E0100301: SJX_Init can't allocate IOP Heap\n");
+                
+                while (TRUE);
+            }
+        }
+
+        sjx_iopwk = (void*)(((Uint32)sjx_iopwk + 64) & ~0x3F);
+        
+        sjx_dtx = DTX_Create(0, sjx_eewk, sjx_iopwk, sjx_wklen);
+
+        if (sjx_dtx == NULL) 
+        {
+            printf("E0100302: SJX_Init can't create DTX\n");
+            
+            while (TRUE);
+        }
+
+        DTX_SetRcvCbf(sjx_dtx, sjx_rcvcbf, 0);
+        DTX_SetSndCbf(sjx_dtx, sjx_sndcbf, 0);
+    }
+
+    sjx_init_cnt++;
 }
 
-// sjx_rcvcbf
+void sjx_rcvcbf(void* sjx, void* buf, Sint32 bfsize)
+{
+    scePrintf("sjx_rcvcbf - UNIMPLEMENTED!\n");
+}
+
 // SJX_Reset
-// sjx_sndcbf
+
+void sjx_sndcbf(void* sjx, void* buf, Sint32 bfsize)
+{
+    scePrintf("sjx_sndcbf - UNIMPLEMENTED!\n");
+}
