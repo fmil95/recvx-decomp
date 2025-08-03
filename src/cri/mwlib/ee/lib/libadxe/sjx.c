83b9d4985ae8e548cdac82bf18171b04fa67c228
Frozen Burnside <fmilesi_soft@hotmail.com>
2025-08-03 18:20:21 -0300

    Decompile SJX_Create

--- src/cri/mwlib/ee/lib/libadxe/sjx.c
+++ src/cri/mwlib/ee/lib/libadxe/sjx.c
@@ -1,3 +1,19 @@
+
+typedef struct _sjx 
+{
+    Sint8   used;
+    Sint8   unk1;
+    Sint8   unk2;
+    Sint8   unk3;
+    SJ      sj;
+    void*   work;
+    Sint32  wksize;
+    Sint32  urpc;
+} SJX_OBJ;
+
+typedef SJX_OBJ    *SJX;
+
+static SJX_OBJ sjx_obj[16];
 static Sint32 sjx_init_cnt;
 static Sint32 sjx_wklen;
 static void* sjx_eewk;
@@ -5,12 +21,56 @@ static Sint32 sjx_ee_work[564];
 static void* sjx_iopwk;
 static DTX sjx_dtx;
 
-void sjx_rcvcbf(void* sjx, void* buf, Sint32 bfsize);
-void sjx_sndcbf(void* sjx, void* buf, Sint32 bfsize);
+void sjx_rcvcbf(SJX sjx, void* buf, Sint32 bfsize);
+void sjx_sndcbf(SJX sjx, void* buf, Sint32 bfsize);
 
-void* SJX_Create(SJ sj, Sint8 *work, Sint32 wksize)
+// 100% matching!
+SJX SJX_Create(SJ sj, Sint8 *work, Sint32 wksize) 
 {
-    scePrintf("SJX_Create - UNIMPLEMENTED!\n");
+    SJX sjx;
+    Sint32 i;
+    Sint32 sbuf[4];
+
+    for (i = 0; i < 16; i++) 
+    {
+        sjx = &sjx_obj[i];
+
+        if (sjx->used == FALSE) 
+        {
+            break;
+        }
+    }
+    
+    if (i == 16) 
+    {
+        return NULL;
+    }
+
+    memset(sjx, 0, sizeof(SJX_OBJ));
+    
+    sjx->wksize = wksize;
+    
+    sjx->sj = sj;
+    
+    sjx->work = work;
+    
+    sbuf[0] = (Sint32)sj;
+    sbuf[1] = (Sint32)work;
+    sbuf[2] = wksize;
+    sbuf[3] = (Sint32)sjx;
+    
+    sjx->urpc = DTX_CallUrpc(0, sbuf, 4, sbuf, 1);
+    
+    if (sjx->urpc == 0) 
+    {
+        printf("E0100401: can't create SJX of IOP\n");
+        
+        return NULL;
+    }
+
+    sjx->used = TRUE;
+    
+    return sjx;
 }
 
 void SJX_Destroy(void* sjx)
@@ -71,14 +131,14 @@ void SJX_Init(void)
     sjx_init_cnt++;
 }
 
-void sjx_rcvcbf(void* sjx, void* buf, Sint32 bfsize)
+void sjx_rcvcbf(SJX sjx, void* buf, Sint32 bfsize)
 {
     scePrintf("sjx_rcvcbf - UNIMPLEMENTED!\n");
 }
 
 // SJX_Reset
 
-void sjx_sndcbf(void* sjx, void* buf, Sint32 bfsize)
+void sjx_sndcbf(SJX sjx, void* buf, Sint32 bfsize)
 {
     scePrintf("sjx_sndcbf - UNIMPLEMENTED!\n");
 }
