7a8442d5f70ae31c18e50208db5360e40bccdf8c
Frozen Burnside <fmilesi_soft@hotmail.com>
2025-08-05 11:13:19 -0300

    Decompile SJMEM_GetBufPtr

--- src/cri/mwlib/ee/lib/libadxe/sj_mem.c
+++ src/cri/mwlib/ee/lib/libadxe/sj_mem.c
@@ -1,18 +1,18 @@
 
 typedef struct _sjmem 
 {
-    Sint8    unk0;
-    Sint8    unk1;
-    Sint8    unk2;
-    Sint8    unk3;
-    Sint32   unk4;
-    Sint32   unk8;
-    Sint32   unkC;
-    Sint32   unk10;
-    Sint32   unk14;
-    Sint32   unk18;
-    Sint32   unk1C;
-    Sint32   unk20;
+    Sint8   unk0;
+    Sint8   unk1;
+    Sint8   unk2;
+    Sint8   unk3;
+    Sint32  unk4;
+    Sint32  unk8;
+    Sint32  unkC;
+    Sint32  unk10;
+    void*   buf;
+    Sint32  unk18;
+    Sint32  unk1C;
+    Sint32  unk20;
 } SJMEM_OBJ;
 
 typedef SJMEM_OBJ     *SJMEM;
@@ -41,7 +41,12 @@ void SJMEM_Finish(void)
     scePrintf("SJMEM_Finish - UNIMPLEMENTED!\n");
 }
 
-// SJMEM_GetBufPtr
+// 100% matching!
+void* SJMEM_GetBufPtr(SJMEM sjmem) 
+{
+    return sjmem->buf;
+}
+
 // SJMEM_GetBufSize
 // SJMEM_GetChunk
 // SJMEM_GetNumData
