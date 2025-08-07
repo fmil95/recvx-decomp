d6d6dc15eb0c944d28c0486f07e85d11dd739307
Frozen Burnside <fmilesi_soft@hotmail.com>
2025-08-07 18:55:10 -0300

    Create sj_rbf header + define file rodata

--- src/cri/mwlib/ee/lib/libadxe/sj_rbf.c
+++ src/cri/mwlib/ee/lib/libadxe/sj_rbf.c
@@ -1,44 +1,9 @@
+#include "sj_rbf.h"
 
-typedef void (*SJRBF_ERRFN)(void *obj, Sint32 errcode);
-
-typedef struct _sjrbf 
-{
-    SJ           sj;
-    Sint32       used;
-    UUID*        uuid;
-    Sint32       datano;
-    Sint32       unk10;
-    Sint32       unk14;
-    Sint32       unk18;
-    void*        buf;
-    Sint32       bfsize;
-    Sint32       xtrsize;
-    SJRBF_ERRFN  err_func;
-    void*        err_obj;
-} SJRBF_OBJ;
-
-typedef SJRBF_OBJ     *SJRBF;
-
-SJ SJRBF_Create(Sint8 *buf, Sint32 bsize, Sint32 xsize);
-void SJRBF_Destroy(SJRBF sjrbf);
-void SJRBF_EntryErrFunc(SJRBF sjrbf, SJRBF_ERRFN func, void* obj);
-void SJRBF_Error(void);
-void SJRBF_Finish(void);
-void* SJRBF_GetBufPtr(SJRBF sjrbf);
-Sint32 SJRBF_GetBufSize(SJRBF sjrbf);
-void SJRBF_GetChunk(SJRBF sjrbf, Sint32 id, Sint32 nbyte, SJCK *ck);
-Sint32 SJRBF_GetNumData(SJRBF sjrbf, Sint32 id);
-UUID* SJRBF_GetUuid(SJRBF sjrbf);
-Sint32 SJRBF_GetXtrSize(SJRBF sjrbf);
-void SJRBF_Init(void);
-Sint32 SJRBF_IsGetChunk(SJRBF sjrbf, Sint32 id, Sint32 nbyte, Sint32 *rbyte);
-void SJRBF_PutChunk(SJRBF sjrbf, Sint32 id, SJCK *ck);
-void SJRBF_Reset(SJRBF sjrbf);  
-void SJRBF_UngetChunk(SJRBF sjrbf, Sint32 id, SJCK *ck);
-
+static Char8* volatile sj_build = "\nSJ Ver.6.00 Build:Jan 26 2001 09:59:32\n";
 static SJRBF_OBJ sjrbf_obj[64] = { 0 };
 static SJ_IF sjrbf_vtbl = { NULL, NULL, NULL, (void*)SJRBF_Destroy, (void*)SJRBF_GetUuid, (void*)SJRBF_Reset, (void*)SJRBF_GetChunk,(void*)SJRBF_UngetChunk, (void*)SJRBF_PutChunk, (void*)SJRBF_GetNumData, (void*)SJRBF_IsGetChunk, (void*)SJRBF_EntryErrFunc };
-static UUID sjrbf_uuid;
+static UUID sjrbf_uuid = { 0x3B9A9E81, 0x0DBB, 0x11D2, { 0xA6, 0xBF, 0x44, 0x45, 0x53, 0x54, 0x00, 0x00 } }; 
 static Sint32 sjrbf_init_cnt;
 
 // 100% matching!
---
+++ src/cri/mwlib/ee/lib/libadxe/sj_rbf.h
@@ -0,0 +1,41 @@
+#ifndef _SJ_RBF_H_
+#define _SJ_RBF_H_
+
+typedef void (*SJRBF_ERRFN)(void *obj, Sint32 errcode);
+
+typedef struct _sjrbf 
+{
+    SJ           sj;
+    Sint32       used;
+    UUID*        uuid;
+    Sint32       datano;
+    Sint32       unk10;
+    Sint32       unk14;
+    Sint32       unk18;
+    void*        buf;
+    Sint32       bfsize;
+    Sint32       xtrsize;
+    SJRBF_ERRFN  err_func;
+    void*        err_obj;
+} SJRBF_OBJ;
+
+typedef SJRBF_OBJ     *SJRBF;
+
+SJ SJRBF_Create(Sint8 *buf, Sint32 bsize, Sint32 xsize);
+void SJRBF_Destroy(SJRBF sjrbf);
+void SJRBF_EntryErrFunc(SJRBF sjrbf, SJRBF_ERRFN func, void* obj);
+void SJRBF_Error(void);
+void SJRBF_Finish(void);
+void* SJRBF_GetBufPtr(SJRBF sjrbf);
+Sint32 SJRBF_GetBufSize(SJRBF sjrbf);
+void SJRBF_GetChunk(SJRBF sjrbf, Sint32 id, Sint32 nbyte, SJCK *ck);
+Sint32 SJRBF_GetNumData(SJRBF sjrbf, Sint32 id);
+UUID* SJRBF_GetUuid(SJRBF sjrbf);
+Sint32 SJRBF_GetXtrSize(SJRBF sjrbf);
+void SJRBF_Init(void);
+Sint32 SJRBF_IsGetChunk(SJRBF sjrbf, Sint32 id, Sint32 nbyte, Sint32 *rbyte);
+void SJRBF_PutChunk(SJRBF sjrbf, Sint32 id, SJCK *ck);
+void SJRBF_Reset(SJRBF sjrbf);  
+void SJRBF_UngetChunk(SJRBF sjrbf, Sint32 id, SJCK *ck);
+
+#endif
