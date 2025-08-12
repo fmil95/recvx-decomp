cca5233eabd0d9b03d1b1b83c09c0327b5e6475e
Frozen Burnside <fmilesi_soft@hotmail.com>
2025-08-12 12:52:36 -0300

    Create sj_uni header

--- src/cri/mwlib/ee/lib/libadxe/sj_uni.c
+++ src/cri/mwlib/ee/lib/libadxe/sj_uni.c
@@ -1,48 +1,4 @@
-
-typedef struct SJUNI_CKLIST 
-{
-    struct SJUNI_CKLIST*  next; 
-    struct SJUNI_CKLIST*  prev; /* unused */ 
-    SJCK                  ck; 
-} SJUNI_CKLIST_OBJ;
-
-typedef SJUNI_CKLIST_OBJ *SJUNI_CKLIST;
-
-typedef void (*SJUNI_ERRFN)(void *obj, Sint32 errcode);
-
-typedef struct _sjuni 
-{
-    SJ_OBJ        sj;
-    Sint8         used;
-    Sint8         mode;
-    Sint8         unk6;      /* unused */ 
-    Sint8         unk7;      /* unused */ 
-    UUID*         uuid;
-    void*         work;
-    Sint32        wksize;
-    void*         data;
-    SJUNI_CKLIST  cklist[4];
-    SJUNI_ERRFN   err_func;
-    void*         err_obj;
-} SJUNI_OBJ;
-
-typedef SJUNI_OBJ *SJUNI;
-
-SJ SJUNI_Create(Sint32 mode, Sint8 *work, Sint32 wksize);
-void SJUNI_Destroy(SJUNI sjuni);
-void SJUNI_EntryErrFunc(SJUNI sjuni, SJUNI_ERRFN func, void* obj);
-void SJUNI_Error(void);
-void SJUNI_Finish(void);
-void SJUNI_GetChunk(SJUNI sjuni, Sint32 id, Sint32 nbyte, SJCK *ck);
-Sint32 SJUNI_GetNumChainPool(SJ sj);
-Sint32 SJUNI_GetNumChunk(SJ sj, Sint32 id);
-Sint32 SJUNI_GetNumData(SJUNI sjuni, Sint32 id);
-UUID* SJUNI_GetUuid(SJUNI sjuni);
-void SJUNI_Init(void);
-Sint32 SJUNI_IsGetChunk(SJUNI sjuni, Sint32 id, Sint32 nbyte, Sint32 *rbyte);
-void SJUNI_PutChunk(SJUNI sjuni, Sint32 id, SJCK *ck);
-void SJUNI_Reset(SJUNI sjuni);   
-void SJUNI_UngetChunk(SJUNI sjuni, Sint32 id, SJCK *ck);
+#include "sj_uni.h"
 
 static SJUNI_OBJ sjuni_obj[64] = { 0 };
 static SJ_IF sjuni_vtbl = { NULL, NULL, NULL, (void*)SJUNI_Destroy, (void*)SJUNI_GetUuid, (void*)SJUNI_Reset, (void*)SJUNI_GetChunk,(void*)SJUNI_UngetChunk, (void*)SJUNI_PutChunk, (void*)SJUNI_GetNumData, (void*)SJUNI_IsGetChunk, (void*)SJUNI_EntryErrFunc };
