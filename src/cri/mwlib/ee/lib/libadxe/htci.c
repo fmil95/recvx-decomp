78ab062a151ad6d84ee177cf33bf3b86a1e94c15
Frozen Burnside <fmilesi_soft@hotmail.com>
2025-08-19 13:14:12 -0300

    Decompile htCiExecHndl

--- src/cri/mwlib/ee/lib/libadxe/htci.c
+++ src/cri/mwlib/ee/lib/libadxe/htci.c
@@ -13,8 +13,8 @@ typedef struct _htci_obj
     Sint32  unk10;
     Sint32  unk14;
     Sint32  tell;
-    Sint32  unk1C;
-    Sint32  unk20;
+    void*   buf;
+    Sint32  nbyte;
 } HTCI_OBJ;
 
 typedef HTCI_OBJ *HTCI;
@@ -209,9 +209,41 @@ void htCiEntryErrFunc(HTCI_ERRFN func, void* obj)
     htg_ci_err_obj = obj;
 }
 
+// 100% matching!
 void htCiExecHndl(HTCI htci)
 {
-    scePrintf("htCiExecHndl - UNIMPLEMENTED!\n");
+    Sint8 stat;
+
+    stat = htci->stat;
+    
+    if ((stat == 2) && (htci_is_all_excute() != 0) && (htci->isend == 0))
+    {
+        if (htci->unk3 == 0) 
+        {
+            htci->unk3 = 1;
+        }
+        else if (htci->unk3 == 1) 
+        {
+            htci->unk3 = stat;
+        }
+        else if (htci->unk3 == stat) 
+        {
+            htci->stat = 1;
+            
+            htci->unk3 = 0;
+            
+            htci->tell += htci->nbyte;
+        }
+
+        if (htci->unk3 == 1)  
+        {        
+            sceLseek(htci->fd, htci->tell * 2048, 0);
+        }
+        else if (htci->unk3 == 2) 
+        {
+            sceRead(htci->fd, htci->buf, htci->nbyte * 2048);
+        }
+    }
 }
 
 // 100% matching!
