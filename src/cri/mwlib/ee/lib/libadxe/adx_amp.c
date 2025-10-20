7c830d379027d93e4a9baee2ec8bcbc867e93055
Frozen Burnside <frozenburnside@hotmail.com>
2025-10-20 16:18:28 -0300

    Decompile ADXAMP_GetExtractNumSmpl

--- src/cri/mwlib/ee/lib/libadxe/adx_amp.c
+++ src/cri/mwlib/ee/lib/libadxe/adx_amp.c
@@ -1,3 +1,22 @@
+
+typedef struct 
+{ 
+	Sint8 used;
+	Sint8 stat;
+	Sint8 maxnch;
+	Sint8 rsrv;
+	SJ sji[2];
+	SJ sjo[2];
+	Sint32 total_exsmpl[2];
+	Sint32 nch;
+	Sint32 sfreq;
+	Float32 frm_len;
+	Float32 frm_prd;
+	Sint32 frm_no;
+} ADX_AMP;
+
+typedef ADX_AMP *ADXAMP;
+
 // ADXAMP_Create
 
 void ADXAMP_Destroy(void* adxamp)
@@ -9,7 +28,13 @@ void ADXAMP_Destroy(void* adxamp)
 // ADXAMP_ExecServer
 // adxamp_extract
 // ADXAMP_Finish
-// ADXAMP_GetExtractNumSmpl
+
+// 100% matching! 
+Sint32 ADXAMP_GetExtractNumSmpl(ADXAMP amp) 
+{
+    return amp->total_exsmpl[0];
+}
+
 // ADXAMP_GetFrmLen
 // ADXAMP_GetFrmPrd
 // ADXAMP_GetSfreq
