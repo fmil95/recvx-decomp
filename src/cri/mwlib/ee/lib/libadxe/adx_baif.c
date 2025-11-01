205dece881b1433011e112e617db69375fdf360e
Frozen Burnside <frozenburnside@hotmail.com>
2025-11-01 20:20:16 -0300

    Decompile ADXB_ExecOneAiff

--- src/cri/mwlib/ee/lib/libadxe/adx_baif.c
+++ src/cri/mwlib/ee/lib/libadxe/adx_baif.c
@@ -1,5 +1,8 @@
 #include "adx_baif.h"
 
+void ADXB_ExecOneAiff8(ADXB adxb);
+void ADXB_ExecOneAiff16(ADXB adxb);
+
 // ADX_DecodeInfoAiff
 
 Sint32 ADXB_CheckAiff(Sint8 *ibuf)
@@ -12,11 +15,27 @@ Sint32 ADXB_DecodeHeaderAiff(ADXB adxb, Sint8 *ibuf, Sint32 ibuflen)
     scePrintf("ADXB_DecodeHeaderAiff - UNIMPLEMENTED!\n");
 }
 
+// 100% matching!
 void ADXB_ExecOneAiff(ADXB adxb)
 {
-    scePrintf("ADXB_ExecOneAiff - UNIMPLEMENTED!\n");
+    if (adxb->cdctype == 1)
+    {
+        ADXB_ExecOneAiff8(adxb);
+    }
+    else 
+    {
+        ADXB_ExecOneAiff16(adxb);
+    }
 }
 
-// ADXB_ExecOneAiff8
-// ADXB_ExecOneAiff16
+void ADXB_ExecOneAiff8(ADXB adxb)
+{
+    scePrintf("ADXB_ExecOneAiff8 - UNIMPLEMENTED!\n");
+}
+
+void ADXB_ExecOneAiff16(ADXB adxb)
+{
+    scePrintf("ADXB_ExecOneAiff16 - UNIMPLEMENTED!\n");
+}
+
 // AIFF_GetInfo
