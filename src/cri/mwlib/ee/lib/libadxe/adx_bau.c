338f451e36bb841b1ba5a5f03c30551cbe609ed7
Frozen Burnside <frozenburnside@hotmail.com>
2025-11-01 20:42:02 -0300

    Decompile ADXB_ExecOneAu

--- src/cri/mwlib/ee/lib/libadxe/adx_bau.c
+++ src/cri/mwlib/ee/lib/libadxe/adx_bau.c
@@ -1,5 +1,9 @@
 #include "adx_bau.h"
 
+void ADXB_ExecOneAu16(ADXB adxb);                          
+void ADXB_ExecOneAu8(ADXB adxb);                            
+void ADXB_ExecOneAuUlaw(ADXB adxb); 
+
 // ADX_DecodeInfoAu
 
 Sint32 ADXB_CheckAu(Sint8 *ibuf)
@@ -12,12 +16,36 @@ Sint32 ADXB_DecodeHeaderAu(ADXB adxb, Sint8 *ibuf, Sint32 ibuflen)
     scePrintf("ADXB_DecodeHeaderAu - UNIMPLEMENTED!\n");
 }
 
+// 100% matching!
 void ADXB_ExecOneAu(ADXB adxb)
 {
-    scePrintf("ADXB_ExecOneAu - UNIMPLEMENTED!\n");
+    if (adxb->cdctype == 2) 
+    {
+        ADXB_ExecOneAuUlaw(adxb);
+    } 
+    else if (adxb->cdctype == 1)
+    {
+        ADXB_ExecOneAu8(adxb);
+    } 
+    else
+    {
+        ADXB_ExecOneAu16(adxb);
+    }
 }
 
-// ADXB_ExecOneAu8
-// ADXB_ExecOneAu16
-// ADXB_ExecOneAuUlaw
+void ADXB_ExecOneAu8(ADXB adxb)
+{
+    scePrintf("ADXB_ExecOneAu8 - UNIMPLEMENTED!\n");
+}
+
+void ADXB_ExecOneAu16(ADXB adxb)
+{
+    scePrintf("ADXB_ExecOneAu16 - UNIMPLEMENTED!\n");
+}
+
+void ADXB_ExecOneAuUlaw(ADXB adxb)
+{
+    scePrintf("ADXB_ExecOneAuUlaw - UNIMPLEMENTED!\n");
+}
+
 // AU_GetInfo
