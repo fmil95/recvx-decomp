063f6db6f40fe84c8b138bfed1e19edd9921dd5d
Frozen Burnside <frozenburnside@hotmail.com>
2025-11-02 10:04:20 -0300

    Decompile ADX_DecodeInfoAiff

--- src/cri/mwlib/ee/lib/libadxe/adx_baif.c
+++ src/cri/mwlib/ee/lib/libadxe/adx_baif.c
@@ -2,8 +2,54 @@
 
 void ADXB_ExecOneAiff8(ADXB adxb);
 void ADXB_ExecOneAiff16(ADXB adxb);
+static void* AIFF_GetInfo(void *hdr, Sint32 *sfreq, Sint32 *nch, Sint32 *bps, Sint32 *nsmpl);
 
-// ADX_DecodeInfoAiff
+// 100% matching!
+Sint32 ADX_DecodeInfoAiff(Sint8 *ibuf, Sint32 ibuflen, Sint16 *dlen, Sint8 *code, Sint8 *bps, Sint8 *blksize, Sint8 *nch, Sint32 *sfreq, Sint32 *total_nsmpl, Sint32 *nsmpl_blk)
+{
+	Sint8 *dt;
+	Sint32 lsfreq;
+	Sint32 lnch;
+	Sint32 lbps;
+	Sint32 lnsmpl;
+    
+    if (ibuflen < 4096) 
+    {
+        *dlen = 0;
+        
+        return -1;
+    }
+    
+    dt = AIFF_GetInfo(ibuf, &lsfreq, &lnch, &lbps, &lnsmpl);
+    
+    if (dt == NULL) 
+    {
+        return -1;
+    }
+
+    *dlen = dt - ibuf;
+    
+    if ((*dlen * 1024) <= 0)
+    {
+        return -1;
+    }
+    
+    *sfreq = lsfreq;
+    
+    *nch = lnch;
+    
+    *bps = lbps;
+    
+    *total_nsmpl = lnsmpl;
+    
+    *code = -1;
+    
+    *blksize = (*nch * *bps) / 8;
+    
+    *nsmpl_blk = 1;
+
+    return 0;
+}
 
 // 100% matching!
 Sint32 ADXB_CheckAiff(Sint8 *ibuf)
@@ -44,4 +90,7 @@ void ADXB_ExecOneAiff16(ADXB adxb)
     scePrintf("ADXB_ExecOneAiff16 - UNIMPLEMENTED!\n");
 }
 
-// AIFF_GetInfo
+static void* AIFF_GetInfo(void *hdr, Sint32 *sfreq, Sint32 *nch, Sint32 *bps, Sint32 *nsmpl)
+{
+    scePrintf("AIFF_GetInfo - UNIMPLEMENTED!\n");
+}
