f6a82c6a70ead0803a26c2e1cd01af403b42f8f4
Frozen Burnside <frozenburnside@hotmail.com>
2025-11-07 19:08:05 -0300

    Decompile ADXB_ExecOneWav

--- src/cri/mwlib/ee/lib/libadxe/adx_bwav.c
+++ src/cri/mwlib/ee/lib/libadxe/adx_bwav.c
@@ -1,5 +1,9 @@
 #include "adx_bwav.h"
 
+void ADXB_ExecOneWav4(ADXB adxb);
+void ADXB_ExecOneWav8(ADXB adxb);
+void ADXB_ExecOneWav16(ADXB adxb);
+
 // ADX_DecodeInfoWav
 
 Sint32 ADXB_CheckWav(Sint8 *ibuf)
@@ -12,11 +16,34 @@ Sint32 ADXB_DecodeHeaderWav(ADXB adxb, Sint8 *ibuf, Sint32 ibuflen)
     scePrintf("ADXB_DecodeHeaderWav - UNIMPLEMENTED!\n");
 }
 
+// 100% matching!
 void ADXB_ExecOneWav(ADXB adxb)
 {
-    scePrintf("ADXB_ExecOneWav - UNIMPLEMENTED!\n");
+    if (adxb->cdctype == 2) 
+    {                    
+        ADXB_ExecOneWav4(adxb);
+    }
+    else if (adxb->cdctype == 1) 
+    {
+        ADXB_ExecOneWav8(adxb);
+    }
+    else if (adxb->cdctype == 0)
+    { 
+        ADXB_ExecOneWav16(adxb);
+    }
 }
 
-// ADXB_ExecOneWav4
-// ADXB_ExecOneWav8
-// ADXB_ExecOneWav16
+void ADXB_ExecOneWav4(ADXB adxb)
+{
+    scePrintf("ADXB_ExecOneWav4 - UNIMPLEMENTED!\n");
+}
+
+void ADXB_ExecOneWav8(ADXB adxb)
+{
+    scePrintf("ADXB_ExecOneWav8 - UNIMPLEMENTED!\n");
+}
+
+void ADXB_ExecOneWav16(ADXB adxb)
+{
+    scePrintf("ADXB_ExecOneWav16 - UNIMPLEMENTED!\n");
+}
