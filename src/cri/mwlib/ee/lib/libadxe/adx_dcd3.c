83b3bf6d78385ef2b944e28c2009c15ed5a917a4
Frozen Burnside <frozenburnside@hotmail.com>
2025-11-08 19:24:07 -0300

    Decompile ADX_DecodeSteFloat

--- src/cri/mwlib/ee/lib/libadxe/adx_dcd3.c
+++ src/cri/mwlib/ee/lib/libadxe/adx_dcd3.c
@@ -7,13 +7,26 @@ Sint32 ADX_DecodeMonoFloat(Sint8 *ibuf, Sint32 nblk, Sint16 *obuf_l, Sint16 *dly
     scePrintf("ADX_DecodeMonoFloat - UNIMPLEMENTED!\n");
 }
 
+// 100% matching!
 Sint32 ADX_DecodeSteFloat(Sint8 *ibuf, Sint32 nblk, Sint16 *obuf_l, Sint16 *dly_l, Sint16 *obuf_r, Sint16 *dly_r, Sint16 k0, Sint16 k1)
 {
-    scePrintf("ADX_DecodeSteFloat - UNIMPLEMENTED!\n");
+    if (adx_decode_output_mono_flag == 0)
+    {
+        return ADX_DecodeSteFloatAsSte(ibuf, nblk, obuf_l, dly_l, obuf_r, dly_r, k0, k1);
+    }
+    
+    return ADX_DecodeSteFloatAsMono(ibuf, nblk, obuf_l, dly_l, obuf_r, dly_r, k0, k1);
 }
 
-// ADX_DecodeSteFloatAsMono
-// ADX_DecodeSteFloatAsSte
+Sint32 ADX_DecodeSteFloatAsMono(Sint8 *ibuf, Sint32 nblk, Sint16 *obuf_l, Sint16 *dly_l, Sint16 *obuf_r, Sint16 *dly_r, Sint16 k0, Sint16 k1) 
+{
+    scePrintf("ADX_DecodeSteFloatAsMono - UNIMPLEMENTED!\n");
+}
+
+Sint32 ADX_DecodeSteFloatAsSte(Sint8 *ibuf, Sint32 nblk, Sint16 *obuf_l, Sint16 *dly_l, Sint16 *obuf_r, Sint16 *dly_r, Sint16 k0, Sint16 k1) 
+{
+    scePrintf("ADX_DecodeSteFloatAsSte - UNIMPLEMENTED!\n");
+}
 
 // 100% matching!
 void ADX_SetDecodeSteAsMonoSw(Sint32 flag)
--- src/cri/mwlib/ee/lib/libadxe/adx_dcd3.h
+++ src/cri/mwlib/ee/lib/libadxe/adx_dcd3.h
@@ -5,6 +5,8 @@
 
 Sint32 ADX_DecodeMonoFloat(Sint8 *ibuf, Sint32 nblk, Sint16 *obuf_l, Sint16 *dly_l, Sint16 k0, Sint16 k1);
 Sint32 ADX_DecodeSteFloat(Sint8 *ibuf, Sint32 nblk, Sint16 *obuf_l, Sint16 *dly_l, Sint16 *obuf_r, Sint16 *dly_r, Sint16 k0, Sint16 k1);
+Sint32 ADX_DecodeSteFloatAsMono(Sint8 *ibuf, Sint32 nblk, Sint16 *obuf_l, Sint16 *dly_l, Sint16 *obuf_r, Sint16 *dly_r, Sint16 k0, Sint16 k1);
+Sint32 ADX_DecodeSteFloatAsSte(Sint8 *ibuf, Sint32 nblk, Sint16 *obuf_l, Sint16 *dly_l, Sint16 *obuf_r, Sint16 *dly_r, Sint16 k0, Sint16 k1);
 void ADX_SetDecodeSteAsMonoSw(Sint32 flag);
 
 #endif
