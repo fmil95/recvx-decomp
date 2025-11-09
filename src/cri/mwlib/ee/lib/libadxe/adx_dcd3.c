6b86b7ca8d47b5c67cc6d2a0301329b31b07a27c
Frozen Burnside <frozenburnside@hotmail.com>
2025-11-09 09:54:41 -0300

    Decompile ADX_DecodeSteFloatAsSte
    Co-Authored-By: Mc-muffin <8714476+Mc-muffin@users.noreply.github.com>

--- src/cri/mwlib/ee/lib/libadxe/adx_dcd3.c
+++ src/cri/mwlib/ee/lib/libadxe/adx_dcd3.c
@@ -160,9 +160,124 @@ Sint32 ADX_DecodeSteFloatAsMono(Sint8 *ibuf, Sint32 nblk, Sint16 *obuf_l, Sint16
     scePrintf("ADX_DecodeSteFloatAsMono - UNIMPLEMENTED!\n");
 }
 
+// 100% matching!
 Sint32 ADX_DecodeSteFloatAsSte(Sint8 *ibuf, Sint32 nblk, Sint16 *obuf_l, Sint16 *dly_l, Sint16 *obuf_r, Sint16 *dly_r, Sint16 k0, Sint16 k1) 
 {
-    scePrintf("ADX_DecodeSteFloatAsSte - UNIMPLEMENTED!\n");
+	Sint32 qsig;
+	Sint32 qsig_r;
+	Sint32 dt16;
+	Float32 gain;
+	Float32 gain_r;
+	Float32 kf0;
+	Float32 kf1;
+	Float32 d0_l;
+	Float32 d1_l;
+	Float32 d0_r;
+	Float32 d1_r;
+	Float32 qsigf;
+	Float32 qsigf_r;
+	Sint32 sno;
+	Sint32 blkno2;
+	Uint8 *ibufdt;
+	Uint8 *ibufdt_r;
+	Sint16 code;
+    Float32 dt_l;
+    Float32 dt_r;
+
+    d0_l = dly_l[0] * 65536;
+    d1_l = dly_l[1] * 65536;
+    
+    d0_r = dly_r[0] * 65536;
+    d1_r = dly_r[1] * 65536;
+    
+    kf0 = (k0 * 16) / 65536.0f;
+    kf1 = (k1 * 16) / 65536.0f;
+    
+    ibufdt_r = (Uint8*)ibuf;
+    
+    for (blkno2 = 0; blkno2 < (nblk / 2); blkno2++) 
+    {
+        ibufdt = ibufdt_r;
+        
+        ibufdt_r += 18;
+        
+        code = BSWAP_U16_EX(*(Uint16*)ibufdt);
+        
+        if ((code & 0x8000)) 
+        {
+            return blkno2 * 2;
+        }
+        
+        gain = (code & 0x1FFF) + 1;
+        
+        code = BSWAP_U16_EX(*(Uint16*)ibufdt_r);
+        
+        if ((code & 0x8000))
+        {
+            return blkno2 * 2;
+        }
+
+        gain_r = (code & 0x1FFF) + 1;
+        
+        ibufdt += 2;
+        ibufdt_r += 2;
+        
+        for (sno = 0; sno < 32; sno += 2)
+        {
+            qsig = *ibufdt++;
+            qsigf = AdxQtblFloat0[qsig];
+            
+            qsig_r = *ibufdt_r++;
+            qsigf_r = AdxQtblFloat0[qsig_r];
+
+            dt_l = (kf0 * d0_l) + (kf1 * d1_l) + (qsigf * gain);
+            
+            d1_l = d0_l;
+            d0_l = dt_l;
+            
+            dt_r = (kf0 * d0_r) + (kf1 * d1_r) + (qsigf_r * gain_r);
+            
+            d1_r = d0_r;
+            d0_r = dt_r;
+            
+            dt16 = d0_l;
+            
+            *obuf_l++ = dt16 >> 16;
+            
+            dt16 = d0_r;
+            
+            *obuf_r++ = dt16 >> 16;
+
+            qsigf = AdxQtblFloat1[qsig];
+            qsigf_r = AdxQtblFloat1[qsig_r];
+            
+            dt_l = (kf0 * d0_l) + (kf1 * d1_l) + (qsigf * gain);
+            
+            d1_l = d0_l;
+            d0_l = dt_l;
+            
+            dt_r = (kf0 * d0_r) + (kf1 * d1_r) + (qsigf_r * gain_r);
+            
+            d1_r = d0_r;
+            d0_r = dt_r;
+            
+            dt16 = d0_l;
+            
+            *obuf_l++ = dt16 >> 16;
+            
+            dt16 = d0_r;
+            
+            *obuf_r++ = dt16 >> 16;
+        }
+    }
+    
+    dly_l[0] = (Sint32)d0_l >> 16;
+    dly_l[1] = (Sint32)d1_l >> 16;
+    
+    dly_r[0] = (Sint32)d0_r >> 16;
+    dly_r[1] = (Sint32)d1_r >> 16;
+    
+    return nblk;
 }
 
 // 100% matching!
