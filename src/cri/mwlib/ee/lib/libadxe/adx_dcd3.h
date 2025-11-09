c454920039b2d6abb0b381fc04bc818f636e0bbb
Frozen Burnside <frozenburnside@hotmail.com>
2025-11-09 09:13:26 -0300

    Decompile ADX_DecodeMonoFloat
    Co-Authored-By: Mc-muffin <8714476+Mc-muffin@users.noreply.github.com>

--- src/cri/mwlib/ee/lib/libadxe/adx_dcd3.h
+++ src/cri/mwlib/ee/lib/libadxe/adx_dcd3.h
@@ -2,6 +2,7 @@
 #define _ADX_DCD3_H_
 
 #include <../../../recvx-decomp-cri/cri/mwlib/ee/include/cri_xpt.h>
+#include "macros.h"
 
 Sint32 ADX_DecodeMonoFloat(Sint8 *ibuf, Sint32 nblk, Sint16 *obuf_l, Sint16 *dly_l, Sint16 k0, Sint16 k1);
 Sint32 ADX_DecodeSteFloat(Sint8 *ibuf, Sint32 nblk, Sint16 *obuf_l, Sint16 *dly_l, Sint16 *obuf_r, Sint16 *dly_r, Sint16 k0, Sint16 k1);
