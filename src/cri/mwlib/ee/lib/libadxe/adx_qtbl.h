caf5afe4c7fd05a0744069d8a03abdfd05bd9667
Frozen Burnside <frozenburnside@hotmail.com>
2025-12-02 16:11:34 -0300

    Create adx_qtbl source file

---
+++ src/cri/mwlib/ee/lib/libadxe/adx_qtbl.h
@@ -0,0 +1,9 @@
+#ifndef _ADX_QTBL_H_
+#define _ADX_QTBL_H_
+
+#include <../../../recvx-decomp-cri/cri/mwlib/ee/include/cri_xpt.h>
+
+extern const Float32 AdxQtblFloat0[256];
+extern const Float32 AdxQtblFloat1[256];
+
+#endif
