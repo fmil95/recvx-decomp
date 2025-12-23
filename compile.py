c6e3114e79f05c1e27a1af9b8fc79e7d90b7fe2e
Mc-muffin <8714476+Mc-muffin@users.noreply.github.com>
2025-12-23 11:28:25 -0500

    Use AS, objdiff pt2

--- compile.py
+++ compile.py
@@ -10,6 +10,12 @@ import splat.scripts.split as split
 from splat.segtypes.linker_entry import LinkerEntry
 
 YAML_FILE = Path("config/SLUS_201.84.yaml")
+SECTION_DICT = {
+    ".text" : ",\"ax\",@progbits,unique,",
+    ".data" : ",\"wa\",@progbits,unique,",
+    ".rodata" : ",\"a\",@progbits,unique,",
+    ".bss" : ",\"wa\",@nobits,unique,",
+}
 
 def load_json(filename):
     """Load JSON data from a file."""
@@ -208,6 +214,51 @@ def src_path_for(source):
     return str(dst_path)
 
 
+def clean_asm(asm_file: Path):
+    with asm_file.open("r", encoding="utf-8") as f:
+        lines = f.readlines()
+
+    if lines[0] == "/* PROCESSED FILE */":
+        return
+    
+    out = ["/* PROCESSED FILE */\n"]
+    skip = False
+    count = 1
+    section = ".text"
+    for line in lines:
+        if line.startswith("/* Automatically generated and unreferenced pad */"):
+            continue
+        
+        if line.startswith("dlabel D_"):
+            skip = True
+            continue
+
+        if line.startswith("enddlabel D_"):
+            skip = True
+            continue
+
+        if line.startswith("glabel") or line.startswith("dlabel"):
+            out.append(f".section {section}{SECTION_DICT[section]}{count}\n")
+            count += 1
+            skip = False
+        
+        if line.startswith("endlabel"):
+            out.append(line)
+            out.append("\n")
+            skip = True
+
+        if line.startswith(".section"):
+            section = line[9:].split(",")[0]
+            count = 1
+            continue
+        
+        if not skip:
+            out.append(line)
+
+    with asm_file.open("w", encoding="utf-8") as f:
+        f.writelines(out)
+
+
 def do_objdiff_setup():
     """Setup objdiff.json and run splat."""
     split.main([YAML_FILE], modes="all", verbose=False, make_full_disasm_for_code=True)
@@ -272,6 +323,10 @@ def do_objdiff_setup():
     with open("objdiff.json", "w", encoding="utf-8") as f:
         json.dump(cfg, f, indent=4)
 
+    for unit in cfg["units"]:
+        if "veronica" in unit["target_path"]:
+            clean_asm(Path(unit["target_path"]).with_suffix(".s"))
+
 
 def main(args):
     """Main entry point for the build process."""
@@ -290,6 +345,8 @@ def main(args):
     compiler_flags = env_vars["compiler_flags"]
     linker_script = env_vars["linker_script"]
     linker_flags = env_vars["linker_flags"]
+    assembler = env_vars.get("assembler", "as")
+    assembler_flags = env_vars["assembler_flags"]
     libraries = env_vars["libs"]
     sources = env_vars["source_files"]
     assembly = env_vars.get("assembly_files", [])
@@ -314,9 +371,7 @@ def main(args):
         "MWLibraryFiles": ""
     }
 
-    assembler = env_vars.get("assembler", "as")
-
-    asm_objects, asm_compile_commands, asm_build_failed = compile_assembly_files(assembler, assembly, [], include_dirs, defines, compiler_env)
+    asm_objects, asm_compile_commands, asm_build_failed = compile_assembly_files(assembler, assembly, assembler_flags, include_dirs, defines, compiler_env)
 
     objects, compile_commands, build_failed = compile_source_files(compiler, sources, compiler_flags, include_dirs, defines, compiler_env)
 
