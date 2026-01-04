20b9f62cd7935a3212bc4506051d77566e361c7e
Frozen Burnside <frozenburnside@hotmail.com>
2026-01-04 13:38:07 -0300

    Define pCnkFuncTbl array + make some edits to ps2_NinjaCnk

--- src/ps2/veronica/prog/ps2_NinjaCnk.h
+++ src/ps2/veronica/prog/ps2_NinjaCnk.h
@@ -4,22 +4,22 @@
 #include "types.h"
 
 void	njInit3D( NJS_VERTEX_BUF *vbuf, Int vn );
-/*void njDrawModel(_anon18* pModel);*/
+void	njDrawModel( NJS_MODEL *model );
 void	njControl3D( Uint32 flag );
-/*void njSetConstantAttr(unsigned int ulAnd, unsigned int ulOr);
-void njSetConstantMaterial(_anon4* pMaterial);
-void njSetCnkBlendMode(unsigned int ulAttr);
-void njCnkModDrawModel(_anon5* pModel);*/
+void	njSetConstantAttr( Uint32 and_attr, Uint32 or_attr );
+void	njSetConstantMaterial( NJS_ARGB *mat );
+void njSetCnkBlendMode(Uint32 attr);
+void    njCnkModDrawModel( NJS_CNK_MODEL *model );
 void    njCnkSetEasyLight( Float x,Float y,Float z );
 void    njCnkSetEasyLightIntensity( Float inten, Float ambient );
 void    njCnkSetEasyLightColor( Float r, Float g, Float b );
-/*void njCnkSetEasyMultiLight(int iLightMax);*/
+void    njCnkSetEasyMultiLight(Int num);
 void    njCnkSetEasyMultiLightSwitch(Int light, Int flag);
 void    njCnkSetEasyMultiAmbient(Float ar, Float ag, Float ab);
 void    njCnkSetEasyMultiLightColor(Int light, Float lr, Float lg, Float lb);
 void    njCnkSetEasyMultiLightVector(Float vx, Float vy, Float vz);
-/*void njCnkSetEasyMultiLightPoint(int iLightNum, float fX, float fY, float fZ);
-void njCnkSetEasyMultiLightRange(int iLightNum, float fNear, float fFar);*/
+void    njCnkSetEasyMultiLightPoint(Int light, Float px, Float py, Float pz);
+void    njCnkSetEasyMultiLightRange(Int light, Float nrange, Float frange);
 void    njCnkSetEasyMultiLightMatrices(void);
 void    njCnkSetSimpleLight( Float x,Float y,Float z );
 void    njCnkSetSimpleLightIntensity( Float inten, Float ambient );
@@ -29,43 +29,43 @@ void    njCnkSetSimpleMultiLightSwitch(Int light, Int flag);
 void    njCnkSetSimpleMultiAmbient(Float ar, Float ag, Float ab);
 void    njCnkSetSimpleMultiLightColor(Int light, Float lr, Float lg, Float lb);
 void    njCnkSetSimpleMultiLightVector(Float vx, Float vy, Float vz);
-/*void njCnkSetSimpleMultiLightPoint(int iLightNum, float fX, float fY, float fZ);
-void njCnkSetSimpleMultiLightRange(int iLightNum, float fNear, float fFar);*/
+void    njCnkSetSimpleMultiLightPoint(Int light, Float px, Float py, Float pz);
+void    njCnkSetSimpleMultiLightRange(Int light, Float nrange, Float frange);
 void    njCnkSetSimpleMultiLightMatrices(void);
-/*void njCnkEasyDrawModel(_anon5* pModel);*/
+void    njCnkEasyDrawModel( NJS_CNK_MODEL *model );
 void    njCnkEasyDrawObject( NJS_CNK_OBJECT *model );
-/*void njCnkEasyMultiDrawModel(_anon5* pModel);*/
+void    njCnkEasyMultiDrawModel( NJS_CNK_MODEL *model );
 void    njCnkEasyMultiDrawObject( NJS_CNK_OBJECT *model );
-/*void njCnkSimpleDrawModel(_anon5* pModel);
-void njCnkSimpleMultiDrawModel(_anon5* pModel);*/
+void    njCnkSimpleDrawModel( NJS_CNK_MODEL *model );
+void    njCnkSimpleMultiDrawModel( NJS_CNK_MODEL *model );
 void njCnkSetCurrentDrawMode(unsigned int ulMode);
 int njCnkDrawModelLocal(NJS_CNK_MODEL* pModel);
-/*int njCnkDrawModelLocalMod(_anon5* pModel);
-tagCHUNK_HEAD* njCnkCn(tagCHUNK_HEAD* pCnk);
-tagCHUNK_HEAD* njCnkCbBa(tagCHUNK_HEAD* pCnk);
-tagCHUNK_HEAD* njCnkCbDa(tagCHUNK_HEAD* pCnk);
-tagCHUNK_HEAD* njCnkCbExp(tagCHUNK_HEAD* pCnk);
-tagCHUNK_HEAD* njCnkCtTid(tagCHUNK_HEAD* pCnk);
-tagCHUNK_HEAD* njCnkCmD(tagCHUNK_HEAD* pCnk);
-tagCHUNK_HEAD* njCnkCmA(tagCHUNK_HEAD* pCnk);
-tagCHUNK_HEAD* njCnkCmDa(tagCHUNK_HEAD* pCnk);
-tagCHUNK_HEAD* njCnkCmS(tagCHUNK_HEAD* pCnk);
-tagCHUNK_HEAD* njCnkCmDs(tagCHUNK_HEAD* pCnk);
-tagCHUNK_HEAD* njCnkCmAs(tagCHUNK_HEAD* pCnk);
-tagCHUNK_HEAD* njCnkCmDas(tagCHUNK_HEAD* pCnk);
-tagCHUNK_HEAD* njCnkCoP4(tagCHUNK_HEAD* pCnk);
-tagCHUNK_HEAD* njCnkCoSt(tagCHUNK_HEAD* pCnk);
-tagCHUNK_HEAD* njCnkCvVn(tagCHUNK_HEAD* pCnk);
-tagCHUNK_HEAD* njCnkCvVnPs2(tagCHUNK_HEAD* pCnk);
-tagCHUNK_HEAD* njCnkCvVMod(tagCHUNK_HEAD* pCnk);
-tagCHUNK_HEAD* njCnkCoP3(tagCHUNK_HEAD* pCnk);
-tagCHUNK_HEAD* njCnkCs(tagCHUNK_HEAD* pCnk);*/
+int njCnkDrawModelLocalMod(NJS_CNK_MODEL* pModel);
+CHUNK_HEAD* njCnkCn(CHUNK_HEAD* pCnk);
+CHUNK_HEAD* njCnkCbBa(CHUNK_HEAD* pCnk);
+CHUNK_HEAD* njCnkCbDa(CHUNK_HEAD* pCnk);
+CHUNK_HEAD* njCnkCbExp(CHUNK_HEAD* pCnk);
+CHUNK_HEAD* njCnkCtTid(CHUNK_HEAD* pCnk);
+CHUNK_HEAD* njCnkCmD(CHUNK_HEAD* pCnk);
+CHUNK_HEAD* njCnkCmA(CHUNK_HEAD* pCnk);
+CHUNK_HEAD* njCnkCmDa(CHUNK_HEAD* pCnk);
+CHUNK_HEAD* njCnkCmS(CHUNK_HEAD* pCnk);
+CHUNK_HEAD* njCnkCmDs(CHUNK_HEAD* pCnk);
+CHUNK_HEAD* njCnkCmAs(CHUNK_HEAD* pCnk);
+CHUNK_HEAD* njCnkCmDas(CHUNK_HEAD* pCnk);
+CHUNK_HEAD* njCnkCoP4(CHUNK_HEAD* pCnk);
+CHUNK_HEAD* njCnkCoSt(CHUNK_HEAD* pCnk);
+CHUNK_HEAD* njCnkCvVn(CHUNK_HEAD* pCnk);
+CHUNK_HEAD* njCnkCvVnPs2(CHUNK_HEAD* pCnk);
+CHUNK_HEAD* njCnkCvVMod(CHUNK_HEAD* pCnk);
+CHUNK_HEAD* njCnkCoP3(CHUNK_HEAD* pCnk);
+CHUNK_HEAD* njCnkCs(CHUNK_HEAD* pCnk);
 int _CVV(float* v0);
-/*tagCHUNK_HEAD* njCnkCsUvh(tagCHUNK_HEAD* pCnk);
-tagCHUNK_HEAD* njCnkCsUvn(tagCHUNK_HEAD* pCnk);*/
+CHUNK_HEAD* njCnkCsUvh(CHUNK_HEAD* pCnk);
+CHUNK_HEAD* njCnkCsUvn(CHUNK_HEAD* pCnk);
 CHUNK_HEAD* njCnkDefaultLong(CHUNK_HEAD* pCnk);
 CHUNK_HEAD* njCnkDefaultShort(CHUNK_HEAD* pCnk);
-/*void njCnkEasyMultiDrawObjectI(cnkobj* pObject);*/
+void njCnkEasyMultiDrawObjectI(NJS_CNK_OBJECT* pObject);
 
 /*typedef struct _anon0;
 typedef struct tagCHUNK_HEAD;
