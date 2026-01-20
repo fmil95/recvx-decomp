092f915bdb06849a888d2e99b1eaf14e3bf7b965
Frozen Burnside <frozenburnside@hotmail.com>
2026-01-19 21:11:46 -0300

    Decompile ExecuteStateSysSaveWriteSysData + reposition some structs
    Co-Authored-By: daru <135557241+davnpsh@users.noreply.github.com>

--- src/ps2/veronica/prog/ps2_sg_maloc.h
+++ src/ps2/veronica/prog/ps2_sg_maloc.h
@@ -4,6 +4,17 @@
 #include "types.h"
 #include "macros.h"
 
+typedef struct HM HM; 
+struct HM
+{
+    // total size: 0x40
+	unsigned int Use_size;    // offset 0x0, size 0x4
+    unsigned int Total_size;  // offset 0x4, size 0x4
+    unsigned int dummy;       // offset 0x8, size 0x4
+	HM* pNext_work;           // offset 0xC, size 0x4
+	unsigned int dummy64[12]; // offset 0x10, size 0x30
+};
+
 void syFree(void* ap);
 void* syMalloc(unsigned int nbytes);
 Void syMallocFinish(Void);
