df09412bc7729b9b0629deb6e7453f3cdbf799bf
Frozen Burnside <frozenburnside@hotmail.com>
2026-02-24 13:25:17 -0300

    Decompile em60_init
    Co-Authored-By: Lajuan Station <69660261+WuGambinos@users.noreply.github.com>
    Co-Authored-By: Valentin Vivaldi <valentin.vivaldi@etendo.software>

--- src/ps2/veronica/prog/subpl.c
+++ src/ps2/veronica/prog/subpl.c
@@ -1,5 +1,6 @@
 #include "subpl.h"
 #include "MdlPut.h"
+#include "Motion.h"
 
 typedef void (*Subpl_proc)(BH_PWORK*);
 Subpl_proc Subpl_tbl[7] = 
@@ -74,34 +75,40 @@ void init_subpl(BH_PWORK* epw)
     subpl_init_tbl[epw->id - 40](epw);
 }
 
-// 
-// Start address: 0x173f60
+// 100% matching! 
 void em60_init(BH_PWORK* epw)
 {
-	// Line 171, Address: 0x173f60, Func Offset: 0
-	// Line 174, Address: 0x173f6c, Func Offset: 0xc
-	// Line 175, Address: 0x173f70, Func Offset: 0x10
-	// Line 177, Address: 0x173f74, Func Offset: 0x14
-	// Line 179, Address: 0x173f7c, Func Offset: 0x1c
-	// Line 184, Address: 0x173f80, Func Offset: 0x20
-	// Line 180, Address: 0x173f84, Func Offset: 0x24
-	// Line 184, Address: 0x173f88, Func Offset: 0x28
-	// Line 185, Address: 0x173f8c, Func Offset: 0x2c
-	// Line 188, Address: 0x173f90, Func Offset: 0x30
-	// Line 186, Address: 0x173f94, Func Offset: 0x34
-	// Line 188, Address: 0x173f98, Func Offset: 0x38
-	// Line 191, Address: 0x173f9c, Func Offset: 0x3c
-	// Line 193, Address: 0x173fac, Func Offset: 0x4c
-	// Line 194, Address: 0x173fbc, Func Offset: 0x5c
-	// Line 195, Address: 0x173fcc, Func Offset: 0x6c
-	// Line 197, Address: 0x173fdc, Func Offset: 0x7c
-	// Line 199, Address: 0x173fec, Func Offset: 0x8c
-	// Line 200, Address: 0x173ffc, Func Offset: 0x9c
-	// Line 201, Address: 0x17400c, Func Offset: 0xac
-	// Line 203, Address: 0x17401c, Func Offset: 0xbc
-	// Line 204, Address: 0x174030, Func Offset: 0xd0
-	// Func End, Address: 0x174040, Func Offset: 0xe0
-	scePrintf("em60_init - UNIMPLEMENTED!\n");
+    epw->hokan_rate = 0;
+    epw->hokan_count = 0;
+    
+    epw->mtn_add = 32768;
+    
+    epw->frm_no = 0;
+    
+    epw->mtn_no = 0;
+    epw->mtn_md = 0x20;
+    epw->mtn_tp = NULL;
+    
+    epw->frm_mode = 0;
+    
+    epw->mode3 = 3;
+
+    if (epw->id == 199) 
+    {
+        epw->mlwP->objP += 8;
+        epw->mlwP->owP += 8;
+        
+        epw->mlwP->obj_num -= 8;
+        
+        bhSetMotion(epw, (int)epw->mtn_add, epw->mtn_md, epw->mtn_tp);
+        
+        epw->mlwP->obj_num += 8;
+        
+        epw->mlwP->owP -= 8;
+        epw->mlwP->objP -= 8;
+    }
+
+    bhSetMotion(epw, (int)epw->mtn_add, epw->mtn_md, epw->mtn_tp);
 }
 
 // 100% matching!
