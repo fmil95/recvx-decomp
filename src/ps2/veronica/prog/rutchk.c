8a22f027e2736b3337f604613441e80649f24273
Frozen Burnside <fmilesi_soft@hotmail.com>
2026-03-06 17:12:46 +0000

    Decompile bhCheckRoute

--- src/ps2/veronica/prog/rutchk.c
+++ src/ps2/veronica/prog/rutchk.c
@@ -1,42 +1,47 @@
 #include "rutchk.h"
 #include "main.h"
 
-/*_anon10* rom;
-_anon15* sys;
-
-unsigned char bhCheckRoute(_anon25* ps0, _anon25* ps1, _anon25* ps2);
-unsigned char bhCheckRouteID(_anon25* ps);
-
-// 
-// Start address: 0x26b8d0
-unsigned char bhCheckRoute(_anon25* ps0, _anon25* ps1, _anon25* ps2)
+// 100% matching! 
+unsigned char bhCheckRoute(NJS_POINT3* ps0, NJS_POINT3* ps1, NJS_POINT3* ps2)
 {
-	unsigned char rid;
-	unsigned char nxt;
-	unsigned char now;
-	_anon0* rp;
-	// Line 69, Address: 0x26b8d0, Func Offset: 0
-	// Line 72, Address: 0x26b8e8, Func Offset: 0x18
-	// Line 73, Address: 0x26b8f4, Func Offset: 0x24
-	// Line 74, Address: 0x26b908, Func Offset: 0x38
-	// Line 75, Address: 0x26b918, Func Offset: 0x48
-	// Line 76, Address: 0x26b92c, Func Offset: 0x5c
-	// Line 78, Address: 0x26b934, Func Offset: 0x64
-	// Line 79, Address: 0x26b93c, Func Offset: 0x6c
-	// Line 80, Address: 0x26b944, Func Offset: 0x74
-	// Line 81, Address: 0x26b948, Func Offset: 0x78
-	// Line 83, Address: 0x26b950, Func Offset: 0x80
-	// Line 85, Address: 0x26b958, Func Offset: 0x88
-	// Line 83, Address: 0x26b960, Func Offset: 0x90
-	// Line 84, Address: 0x26b970, Func Offset: 0xa0
-	// Line 83, Address: 0x26b974, Func Offset: 0xa4
-	// Line 84, Address: 0x26b980, Func Offset: 0xb0
-	// Line 85, Address: 0x26b990, Func Offset: 0xc0
-	// Line 86, Address: 0x26b9a4, Func Offset: 0xd4
-	// Line 87, Address: 0x26b9b8, Func Offset: 0xe8
-	// Line 89, Address: 0x26b9cc, Func Offset: 0xfc
-	// Func End, Address: 0x26b9e4, Func Offset: 0x114
-}*/
+    ATR_WORK* rp;      
+    unsigned char now; 
+    unsigned char nxt; 
+    unsigned char rid; 
+    
+    rid = bhCheckRouteID(ps0);
+    
+    if (rid == 0xFF) 
+    {
+        return 0xFF;
+    }
+    
+    nxt = bhCheckRouteID(ps1);
+
+    if (nxt == 0xFF) 
+    {
+        return 0xFF;
+    }
+    
+    if (rid == nxt) 
+    {
+        ps2->x = ps1->x;
+        ps2->y = ps1->y;
+        ps2->z = ps1->z;
+        
+        return 0xFF;
+    } 
+
+    now = *((rom->ruttp + (rom->rut_n * nxt)) + rid);
+        
+    rp = &rom->rutp[now];
+        
+    ps2->x = rp->px + (0.5f * rp->w);
+    ps2->y = rp->py + (0.5f * rp->h);
+    ps2->z = rp->pz + (0.5f * rp->d);
+    
+    return now;
+}
 
 // 100% matching!
 unsigned char bhCheckRouteID(NJS_POINT3* ps)
