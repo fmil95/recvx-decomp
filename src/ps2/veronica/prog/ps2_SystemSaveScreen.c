b50542324c990c08d845e7bc22863ba88ae533e2
Frozen Burnside <frozenburnside@hotmail.com>
2026-04-08 11:48:34 -0300

    Move ps2_SystemSaveScreen function prototypes to header

--- src/ps2/veronica/prog/ps2_SystemSaveScreen.c
+++ src/ps2/veronica/prog/ps2_SystemSaveScreen.c
@@ -7,57 +7,6 @@
 #include "../../../ps2/veronica/prog/sdfunc.h"
 #include "../../../ps2/veronica/prog/main.h"
 
-SYSSAVE_SCREEN* CreateSysSaveScreen(SYSSAVE_SCREEN* pSysSave, void* vpWorkPtrSys, unsigned short usSaveMesMode, unsigned short usSaveWriteMode);
-void DispSysSaveMessageSelect(SYSSAVE_SCREEN* pSysSave);
-int ExecuteSysSaveScreen(SYSSAVE_SCREEN* pSysSave);
-void SetStateSysSaveAwarenessCard(SYSSAVE_SCREEN* pSysSave);
-void ExecuteStateSysSaveAwarenessCard(SYSSAVE_SCREEN* pSysSave);
-void SetStateSysSaveErrUnPS2MemCard(SYSSAVE_SCREEN* pSysSave);
-void ExecuteStateSysSaveErrUnPS2MemCard(SYSSAVE_SCREEN* pSysSave);
-void SetStateSysSaveErrLostCard(SYSSAVE_SCREEN* pSysSave);
-void ExecuteStateSysSaveErrLostCard(SYSSAVE_SCREEN* pSysSave);
-void SetStateSysSaveErrPort2(SYSSAVE_SCREEN* pSysSave);
-void ExecuteStateSysSaveErrPort2(SYSSAVE_SCREEN* pSysSave);
-void SetStateSysSaveDirCheck(SYSSAVE_SCREEN* pSysSave);
-void ExecuteStateSysSaveDirCheck(SYSSAVE_SCREEN* pSysSave);
-void SetStateSysSaveFreeCapacity(SYSSAVE_SCREEN* pSysSave);
-void ExecuteStateSysSaveFreeCapacity(SYSSAVE_SCREEN* pSysSave);
-void SetStateSysSaveErrFreeCapacity(SYSSAVE_SCREEN* pSysSave);
-void ExecuteStateSysSaveErrFreeCapacity(SYSSAVE_SCREEN* pSysSave);
-void SetStateSysSaveCheckWriteSysData(SYSSAVE_SCREEN* pSysSave);
-void ExecuteStateSysSaveCheckWriteSysData(SYSSAVE_SCREEN* pSysSave);
-void SetStateSysSaveWriteSysData(SYSSAVE_SCREEN* pSysSave);
-void ExecuteStateSysSaveWriteSysData(SYSSAVE_SCREEN* pSysSave);
-void SetStateSysSaveSuccessWriteSysData(SYSSAVE_SCREEN* pSysSave);
-void ExecuteStateSysSaveSuccessWriteSysData(SYSSAVE_SCREEN* pSysSave);
-void SetStateSysSaveErrWriteSysData(SYSSAVE_SCREEN* pSysSave);
-void ExecuteStateSysSaveErrWriteSysData(SYSSAVE_SCREEN* pSysSave);
-void SetStateSysSaveFileBroken(SYSSAVE_SCREEN* pSysSave);
-void ExecuteStateSysSaveFileBroken(SYSSAVE_SCREEN* pSysSave);
-void SetStateSysSaveExitWriteSysData(SYSSAVE_SCREEN* pSysSave);
-void ExecuteStateSysSaveExitWriteSysData(SYSSAVE_SCREEN* pSysSave);
-void SetStateWriteRankingData(SYSSAVE_SCREEN* pSysSave);
-void ExecuteStateWriteRankingData(SYSSAVE_SCREEN* pSysSave);
-void SetStateSysSave(SYSSAVE_SCREEN* pSysSave);
-void ExecuteStateSysSave(SYSSAVE_SCREEN* pSysSave);
-void SetStateSysSaveErrCardWrite(SYSSAVE_SCREEN* pSysSave);
-void ExecuteStateSysSaveErrCardWrite(SYSSAVE_SCREEN* pSysSave);
-void SetStateSysSaveFormat(SYSSAVE_SCREEN* pSysSave);
-void ExecuteStateSysSaveFormat(SYSSAVE_SCREEN* pSysSave);
-void SetStateSysSaveStartFormat(SYSSAVE_SCREEN* pSysSave);
-void ExecuteStateSysSaveStartFormat(SYSSAVE_SCREEN* pSysSave);
-void SetStateSysSaveExitFormat(SYSSAVE_SCREEN* pSysSave);
-void ExecuteStateSysSaveExitFormat(SYSSAVE_SCREEN* pSysSave);
-void SetStateSysSaveSuccessFormat(SYSSAVE_SCREEN* pSysSave);
-void ExecuteStateSysSaveSuccessFormat(SYSSAVE_SCREEN* pSysSave);
-void SetStateSysSaveErrFormat(SYSSAVE_SCREEN* pSysSave);
-void ExecuteStateSysSaveErrFormat(SYSSAVE_SCREEN* pSysSave);
-void SetStateSysSaveTitleExit(SYSSAVE_SCREEN* pSysSave);
-void ExecuteStateSysSaveTitleExit();
-void SysSaveHikaku(CONFIGFILE* pConfig);
-unsigned int GetOkButton();
-unsigned int GetCancelButton();
-
 // 100% matching!
 SYSSAVE_SCREEN* CreateSysSaveScreen(SYSSAVE_SCREEN* pSysSave, void* vpWorkPtrSys, unsigned short usSaveMesMode, unsigned short usSaveWriteMode)
 {
