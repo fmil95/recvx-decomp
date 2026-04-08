6742edde6762713b92584f71b2736b9cea97a17b
Frozen Burnside <frozenburnside@hotmail.com>
2026-04-08 13:58:58 -0300

    Fill gcc_wrapper

--- src/ps2/veronica/prog/gcc_wrapper.c
+++ src/ps2/veronica/prog/gcc_wrapper.c
@@ -1,81 +1,107 @@
+#ifdef __cplusplus
+	#define EXTERN extern "C"
+#else
+	#define EXTERN extern
+#endif
 
 
+/*
+ *	Exported routines
+ *
+ */
+ 
+EXTERN float _f_ulltof(unsigned long long ul);
+EXTERN double _d_utod(unsigned int ui);
+
+EXTERN int _dpfeq(double left, double right);
+EXTERN int _dpfne(double left, double right);
+EXTERN int _dpflt(double left, double right);
+EXTERN int _dpfle(double left, double right);
+EXTERN int _dpfgt(double left, double right);
+EXTERN int _dpfge(double left, double right);
 
 
-double _d_ulltod(unsigned long long ul);
-double _d_utod(unsigned int ui);
-int _dpfne(double left, double right);
-int _dpflt(double left, double right);
-int _dpfle(double left, double right);
-int _dpfgt(double left, double right);
+/*
+ *	Imported routines (GCC)
+ *
+ */
+ 
+EXTERN float __floatdisf(unsigned long long ul);
 
-// 
-// Start address: 0x2e5e60
-double _d_ulltod(unsigned long long ul)
-{
-	double d;
-	// Line 55, Address: 0x2e5e60, Func Offset: 0
-	// Line 58, Address: 0x2e5e64, Func Offset: 0x4
-	// Line 59, Address: 0x2e5e6c, Func Offset: 0xc
-	// Line 61, Address: 0x2e5e7c, Func Offset: 0x1c
-	// Line 62, Address: 0x2e5e8c, Func Offset: 0x2c
-	// Line 64, Address: 0x2e5e98, Func Offset: 0x38
-	// Func End, Address: 0x2e5ea4, Func Offset: 0x44
+EXTERN double __floatdidf(unsigned long long ul);
+EXTERN int dpcmp(double left, double right);
+
+
+EXTERN float _f_ulltof(unsigned long long ul) {
+	signed long long sl = (signed long long) ul;
+	
+	if (sl > 0)	
+		return __floatdisf(ul);
+	else {
+		float f = __floatdisf((ul>>1)|(ul&1));
+		return f+f;
+	}
 }
 
-// 
-// Start address: 0x2e5eb0
-double _d_utod(unsigned int ui)
-{
-	// Line 68, Address: 0x2e5eb0, Func Offset: 0
-	// Func End, Address: 0x2e5ebc, Func Offset: 0xc
+
+
+EXTERN double _d_ulltod(unsigned long long ul) {
+	signed long long sl = (signed long long) ul;
+	
+	if (sl > 0)	
+		return __floatdidf(ul);
+	else {
+		double d = __floatdidf((ul>>1)|(ul&1));
+		return d+d;
+	}
 }
 
-// 
-// Start address: 0x2e5ec0
-int _dpfne(double left, double right)
-{
-	// Line 87, Address: 0x2e5ec0, Func Offset: 0
-	// Line 88, Address: 0x2e5ec8, Func Offset: 0x8
-	// Line 89, Address: 0x2e5ed0, Func Offset: 0x10
-	// Line 88, Address: 0x2e5ed4, Func Offset: 0x14
-	// Line 89, Address: 0x2e5ed8, Func Offset: 0x18
-	// Func End, Address: 0x2e5ee0, Func Offset: 0x20
+
+EXTERN double _d_utod(unsigned int ui) {
+	return _d_ulltod(ui);
 }
 
-// 
-// Start address: 0x2e5ee0
-int _dpflt(double left, double right)
+
+/*
+ *	The GCC runtime has only one float compare function that
+ *	return a number to tell how the float compare.  Our backend
+ *	assume the compare functions will return a boolean.  
+ *	This wrapper makes the intreface between GCC and our backend.
+ *
+ */
+
+EXTERN int _dpfeq(double left, double right)
 {
-	// Line 93, Address: 0x2e5ee0, Func Offset: 0
-	// Line 94, Address: 0x2e5ee8, Func Offset: 0x8
-	// Line 95, Address: 0x2e5ef0, Func Offset: 0x10
-	// Line 94, Address: 0x2e5ef4, Func Offset: 0x14
-	// Line 95, Address: 0x2e5ef8, Func Offset: 0x18
-	// Func End, Address: 0x2e5f00, Func Offset: 0x20
+	return dpcmp(left, right) == 0;
 }
 
-// 
-// Start address: 0x2e5f00
-int _dpfle(double left, double right)
+
+EXTERN int _dpfne(double left, double right)
 {
-	// Line 99, Address: 0x2e5f00, Func Offset: 0
-	// Line 100, Address: 0x2e5f08, Func Offset: 0x8
-	// Line 101, Address: 0x2e5f10, Func Offset: 0x10
-	// Line 100, Address: 0x2e5f14, Func Offset: 0x14
-	// Line 101, Address: 0x2e5f1c, Func Offset: 0x1c
-	// Func End, Address: 0x2e5f24, Func Offset: 0x24
+	return dpcmp(left, right) != 0;
 }
 
-// 
-// Start address: 0x2e5f30
-int _dpfgt(double left, double right)
+
+EXTERN int _dpflt(double left, double right)
 {
-	// Line 105, Address: 0x2e5f30, Func Offset: 0
-	// Line 106, Address: 0x2e5f38, Func Offset: 0x8
-	// Line 107, Address: 0x2e5f40, Func Offset: 0x10
-	// Line 106, Address: 0x2e5f44, Func Offset: 0x14
-	// Line 107, Address: 0x2e5f48, Func Offset: 0x18
-	// Func End, Address: 0x2e5f50, Func Offset: 0x20
+	return dpcmp(left, right) < 0;
+}
+
+
+EXTERN int _dpfle(double left, double right)
+{
+	return dpcmp(left, right) <= 0;
+}
+
+
+EXTERN int _dpfgt(double left, double right)
+{
+	return dpcmp(left, right) > 0;
+}
+
+
+EXTERN int _dpfge(double left, double right)
+{
+	return dpcmp(left, right) >= 0;
 }
 
