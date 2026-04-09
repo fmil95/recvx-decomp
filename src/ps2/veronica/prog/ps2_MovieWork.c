2db28b21f7c0be52c6d63655dfd0467f1fd4ea6e
Frozen Burnside <frozenburnside@hotmail.com>
2026-04-09 13:50:02 -0300

    Add segment definition for ps2_MovieWork on splat config

--- src/ps2/veronica/prog/ps2_MovieWork.c
+++ src/ps2/veronica/prog/ps2_MovieWork.c
@@ -1,12 +1,12 @@
 #include "../../../ps2/veronica/prog/ps2_MovieWork.h"
 
+unsigned char* mpegWork;
 VoTag* voBufTag __attribute__((aligned(64)));
 VoData* voBufData;
-READ_BUF* readBuf;
-unsigned char* audioBuff;
-TimeStamp* timeStamp;
 u_long128* viBufTag;
 u_long128* viBufData;
-unsigned char* mpegWork;
+READ_BUF* readBuf;
+unsigned char* audioBuff;
 char* videoDecStack;
+TimeStamp* timeStamp;
 /*void* vpWorkMemoryTop; - unused */
