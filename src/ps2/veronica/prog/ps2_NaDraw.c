9d2c45800d6fb1cca8038d19cc223e6684bb7596
Frozen Burnside <frozenburnside@hotmail.com>
2026-04-10 12:33:01 -0300

    Decompile njDrawPolygon3DEx

--- src/ps2/veronica/prog/ps2_NaDraw.c
+++ src/ps2/veronica/prog/ps2_NaDraw.c
@@ -221,16 +221,12 @@ void    njDrawTextureH( NJS_TEXTUREH_VTX *polygon, Int count, Int tex, Int flag 
 	scePrintf("njDrawTextureH - UNIMPLEMENTED!\n");
 }
 
-// 
-// Start address: 0x2de740
+// 100% matching!
 void    njDrawPolygon3DEx( NJS_POLYGON_VTX *p, Int count, Int trans )
 {
-	// Line 360, Address: 0x2de740, Func Offset: 0
-	// Line 361, Address: 0x2de758, Func Offset: 0x18
-	// Line 362, Address: 0x2de760, Func Offset: 0x20
-	// Line 364, Address: 0x2de76c, Func Offset: 0x2c
-	// Func End, Address: 0x2de780, Func Offset: 0x40
-	scePrintf("njDrawPolygon3DEx - UNIMPLEMENTED!\n");
+	njDrawPolygon3DExStart(trans);
+
+	njDrawPolygon3DExSetData(p, count);
 }
 
 // 100% matching!
