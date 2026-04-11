3b5073d8d2f19b0e3e459d68f96ad6cbf17fa478
Frozen Burnside <frozenburnside@hotmail.com>
2026-04-11 16:02:01 -0300

    Decompile njDrawTextureSub3D
    Co-Authored-By: Norgesnerd <5824576+Norgesnerd@users.noreply.github.com>

--- include/ps2/veronica/prog/ps2_NaDraw.h
+++ include/ps2/veronica/prog/ps2_NaDraw.h
@@ -3,114 +3,29 @@
 
 #include "types.h"
 
+void    njDrawPolygon( NJS_POLYGON_VTX *polygon, Int count, Int trans );
+void njDrawTextureSub(NJS_TEXTURE_VTX* polygon, int count, int flag);
+void    njDrawTexture( NJS_TEXTURE_VTX *polygon, Int count, Int tex, Int flag );
+void njDrawTextureSubH(NJS_TEXTUREH_VTX* polygon, int count, int flag);
+void    njDrawTextureH( NJS_TEXTUREH_VTX *polygon, Int count, Int tex, Int flag );
+void    njDrawPolygon3DEx( NJS_POLYGON_VTX *p, Int count, Int trans );
+void	njDrawPolygon3DExStart(Int trans);
+void njDrawPolygonSub3D(NJS_POLYGON_VTX* polygon, int count, int flag);
+void    njDrawPolygon3DExSetData( NJS_POLYGON_VTX *p, Int count);
+void	njDrawPolygon3DExEnd(void);
+void	njDrawTexture3DEx( NJS_TEXTURE_VTX *p, Int count, Int trans );
+void njDrawTexture3DEx1P(NJS_TEXTURE_VTX* p, int count, int trans);
+void	njDrawTexture3DExStart(Int trans);
+void njDrawTextureSub3D(NJS_TEXTURE_VTX* polygon, int count, int flag);
+void njDrawTextureSub3D1P(NJS_TEXTURE_VTX* polygon, int count, int flag);
+void	njDrawTexture3DExSetData( NJS_TEXTURE_VTX *p, Int count);
+void njDrawTexture3DExSetData1P(NJS_TEXTURE_VTX* p, int count);
+void	njDrawTexture3DExEnd(void);
+void njDrawTextureSub3DH(NJS_TEXTUREH_VTX* polygon, int count, int flag);
+void njDrawTexture3DHEx(NJS_TEXTUREH_VTX* p, int count, int trans);
 void njQuadTextureStart(int trans);
 void njQuadTextureEnd();
 void njSetQuadTexture(int texid, unsigned int col);
 void njDrawQuadTexture(QUAD* q, float z);
-
-/*typedef struct _anon0;
-typedef struct _anon1;
-typedef struct _anon2;
-typedef struct _anon3;
-typedef struct _anon4;
-typedef struct _anon5;
-typedef struct tagVU1_STRIP_BUF;
-typedef struct tagNJS_SCRVECTOR;
-
-
-typedef _anon0 type_0[32];
-typedef float type_1[4][128];
-typedef float type_2[4];
-typedef float type_3[4][6];
-typedef float type_4[4][64];
-typedef float type_5[4][64];
-typedef tagVU1_STRIP_BUF type_6[256];
-typedef float type_7[4][64];
-typedef _anon0 type_8[4];
-typedef float type_9[4][128];
-typedef float type_10[4][64];
-
-struct _anon0
-{
-	float x;
-	float y;
-	float z;
-	float u;
-	float v;
-	unsigned int bcol;
-	unsigned int ocol;
-};
-
-struct _anon1
-{
-	void* filename;
-	unsigned int attr;
-	unsigned int texaddr;
-};
-
-struct _anon2
-{
-	_anon1* textures;
-	unsigned int nbTexture;
-};
-
-struct _anon3
-{
-	float x;
-	float y;
-	float z;
-	float u;
-	float v;
-	unsigned int col;
-};
-
-struct _anon4
-{
-	float x1;
-	float y1;
-	float x2;
-	float y2;
-	float u1;
-	float v1;
-	float u2;
-	float v2;
-};
-
-struct _anon5
-{
-	float x;
-	float y;
-	float z;
-	unsigned int col;
-};
-
-struct tagVU1_STRIP_BUF
-{
-	float fU;
-	float fV;
-	float fPad0;
-	float fPad1;
-	float fIr;
-	float fIg;
-	float fIb;
-	float fA;
-	float fVx;
-	float fVy;
-	float fVz;
-	float fFog;
-	float fSx;
-	float fSy;
-	float fIz;
-	float fNz;
-};
-
-struct tagNJS_SCRVECTOR
-{
-	float x;
-	float y;
-	float z;
-	float iz;
-	float fog;
-};*/
 
 #endif
--- src/ps2/veronica/prog/ps2_NaDraw.c
+++ src/ps2/veronica/prog/ps2_NaDraw.c
@@ -4,31 +4,6 @@
 #include "../../../ps2/veronica/prog/ps2_Vu1Strip.h"
 #include "../../../ps2/veronica/prog/main.h"
 
-/*void njDrawPolygon(_anon5* polygon, int count, int trans);
-void njDrawTextureSub(NJS_TEXTURE_VTX* polygon, int count, int flag);
-void njDrawTexture(NJS_TEXTURE_VTX* polygon, int count, int tex, int flag);
-void njDrawTextureSubH(NJS_TEXTUREH_VTX* polygon, int count, int flag);
-void njDrawTextureH(NJS_TEXTUREH_VTX* polygon, int count, int tex, int flag);
-void njDrawPolygon3DEx(_anon5* p, int count, int trans);
-void njDrawPolygon3DExStart(int trans);
-void njDrawPolygonSub3D(_anon5* polygon, int count, int flag);
-void njDrawPolygon3DExSetData(_anon5* p, int count);
-void njDrawPolygon3DExEnd();
-void njDrawTexture3DEx(NJS_TEXTURE_VTX* p, int count, int trans);
-void njDrawTexture3DEx1P(NJS_TEXTURE_VTX* p, int count, int trans);
-void njDrawTexture3DExStart(int trans);
-void njDrawTextureSub3D(NJS_TEXTURE_VTX* polygon, int count, int flag);
-void njDrawTextureSub3D1P(NJS_TEXTURE_VTX* polygon, int count, int flag);
-void njDrawTexture3DExSetData(NJS_TEXTURE_VTX* p, int count);*/
-void njDrawTexture3DExSetData1P(NJS_TEXTURE_VTX* p, int count);
-/*void njDrawTexture3DExEnd();
-void njDrawTextureSub3DH(NJS_TEXTUREH_VTX* polygon, int count, int flag);
-void njDrawTexture3DHEx(NJS_TEXTUREH_VTX* p, int count, int trans);
-void njQuadTextureStart(int trans);
-void njQuadTextureEnd();
-void njSetQuadTexture(int texid, unsigned int col);
-void njDrawQuadTexture(_anon4* q, float z);*/
-
 // 100% matching!
 void    njDrawPolygon( NJS_POLYGON_VTX *polygon, Int count, Int trans )
 {
@@ -323,54 +298,63 @@ void	njDrawTexture3DExStart(Int trans)
 	Ps2_tex3DEx_trans = trans;
 }
 
-// 
-// Start address: 0x2de970
+// 100% matching!
 void njDrawTextureSub3D(NJS_TEXTURE_VTX* polygon, int count, int flag)
 {
-	unsigned int cflag;
-	NJS_SCRVECTOR scr;
-	unsigned int col;
-	unsigned int i;
-	float bp[4];
-	float buff[4][64];
-	float invz;
-	// Line 507, Address: 0x2de970, Func Offset: 0
-	// Line 512, Address: 0x2de994, Func Offset: 0x24
-	// Line 520, Address: 0x2de998, Func Offset: 0x28
-	// Line 524, Address: 0x2de9b0, Func Offset: 0x40
-	// Line 546, Address: 0x2de9b4, Func Offset: 0x44
-	// Line 525, Address: 0x2de9bc, Func Offset: 0x4c
-	// Line 526, Address: 0x2de9d0, Func Offset: 0x60
-	// Line 527, Address: 0x2de9e4, Func Offset: 0x74
-	// Line 528, Address: 0x2de9f4, Func Offset: 0x84
-	// Line 546, Address: 0x2dea04, Func Offset: 0x94
-	// Line 548, Address: 0x2dea0c, Func Offset: 0x9c
-	// Line 555, Address: 0x2dea28, Func Offset: 0xb8
-	// Line 560, Address: 0x2dea2c, Func Offset: 0xbc
-	// Line 555, Address: 0x2dea30, Func Offset: 0xc0
-	// Line 556, Address: 0x2dea34, Func Offset: 0xc4
-	// Line 557, Address: 0x2dea3c, Func Offset: 0xcc
-	// Line 558, Address: 0x2dea44, Func Offset: 0xd4
-	// Line 560, Address: 0x2dea48, Func Offset: 0xd8
-	// Line 563, Address: 0x2dea50, Func Offset: 0xe0
-	// Line 564, Address: 0x2dea54, Func Offset: 0xe4
-	// Line 568, Address: 0x2dea58, Func Offset: 0xe8
-	// Line 560, Address: 0x2dea60, Func Offset: 0xf0
-	// Line 564, Address: 0x2dea64, Func Offset: 0xf4
-	// Line 565, Address: 0x2dea6c, Func Offset: 0xfc
-	// Line 567, Address: 0x2dea74, Func Offset: 0x104
-	// Line 565, Address: 0x2dea78, Func Offset: 0x108
-	// Line 566, Address: 0x2dea7c, Func Offset: 0x10c
-	// Line 567, Address: 0x2dea80, Func Offset: 0x110
-	// Line 568, Address: 0x2dea84, Func Offset: 0x114
-	// Line 570, Address: 0x2dea90, Func Offset: 0x120
-	// Line 573, Address: 0x2dea98, Func Offset: 0x128
-	// Line 574, Address: 0x2deaa4, Func Offset: 0x134
-	// Line 576, Address: 0x2deab8, Func Offset: 0x148
-	// Line 577, Address: 0x2deac0, Func Offset: 0x150
-	// Line 583, Address: 0x2dead4, Func Offset: 0x164
-	// Func End, Address: 0x2deaf8, Func Offset: 0x188
-	scePrintf("njDrawTextureSub3D - UNIMPLEMENTED!\n");
+    float invz;       
+    float buff[64][4];  
+    float (*bp)[4];    
+    unsigned int i;     
+    unsigned int col;   
+    NJS_SCRVECTOR scr;  
+    unsigned int cflag; 
+    
+    cflag = 15;
+    
+    bp = buff;
+    
+    for (i = 0; i < count; i++, bp += 3) 
+    {
+        col = polygon[i].col;
+        
+        *(unsigned int*)&bp[1][0] = (((col >> 16) & 0xFF) + 1) >> 1;
+        *(unsigned int*)&bp[1][1] = (((col >>  8) & 0xFF) + 1) >> 1;
+        *(unsigned int*)&bp[1][2] = (((col >>  0) & 0xFF) + 1) >> 1;
+        *(unsigned int*)&bp[1][3] = (((col >> 24) & 0xFF) + 1) >> 1;
+        
+        njRotTransPers((NJS_POINT3*)&polygon[i].x, &scr);
+        
+        if (scr.z < 0) 
+        { 
+            return;
+        }
+        
+        bp[2][0] = scr.x;
+        bp[2][1] = scr.y;
+        bp[2][2] = scr.z;
+        bp[2][3] = scr.fog;
+       
+        cflag &= _Clip_Screen(&scr.x);
+        
+        invz = scr.iz;
+        
+        bp[0][0] = invz * polygon[i].u;
+        bp[0][1] = invz * polygon[i].v;
+        bp[0][2] = invz;
+        bp[0][3] = 0; 
+    }
+    
+    if (cflag == 0) 
+    {
+        if (flag == 1) 
+        {
+            Ps2AddPrim3D(0x3E000000000000, buff, count);
+        }
+        else 
+        {
+            Ps2AddPrim3D(0x1E000000000000, buff, count);
+        }
+    }
 }
 
 // 100% matching!
