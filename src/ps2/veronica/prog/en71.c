6bfadac18ed02ddab9dd549c5a75f7e22a5e6810
Frozen Burnside <frozenburnside@hotmail.com>
2026-04-30 20:37:15 -0300

    Decompile bhEne71_Damage

--- src/ps2/veronica/prog/en71.c
+++ src/ps2/veronica/prog/en71.c
@@ -331,18 +331,16 @@ void bhEne71_Nage()
 {
 	// Line 235, Address: 0x21bb30, Func Offset: 0
 	// Func End, Address: 0x21bb38, Func Offset: 0x8
-}
+}*/
 
-// 
-// Start address: 0x21bb40
+// 100% matching!
 void bhEne71_Damage()
 {
-	// Line 247, Address: 0x21bb40, Func Offset: 0
-	// Func End, Address: 0x21bb48, Func Offset: 0x8
-}*/
+	
+}
 
 // 100% matching!
 void bhEne71_Die()
 {
-	
+
 }
