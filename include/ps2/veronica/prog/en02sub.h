5901b409b84e86fefe65bf5d5a60009d3f0de7ec
Frozen Burnside <frozenburnside@hotmail.com>
2026-05-03 12:51:55 -0300

    Decompile bhEne02sub_MV00

--- include/ps2/veronica/prog/en02sub.h
+++ include/ps2/veronica/prog/en02sub.h
@@ -3,6 +3,13 @@
 
 #include "types.h"
 
-
+void bhEne02sub(BH_PWORK* epw);
+void bhEne02sub_Init(BH_PWORK* epw);
+void bhEne02sub_Move(BH_PWORK* epw);
+void bhEne02sub_MV00();
+void bhEne02sub_MV01(BH_PWORK* epw);
+void bhEne02sub_MV02(BH_PWORK* epw);
+void bhEne02sub_MV03(BH_PWORK* epw);
+void bhEne02sub_MV04(BH_PWORK* epw);
 
 #endif
